1c7e00890a0243c67a36b429c95f82cd920e77db
raoulcollenteur <info@raoulcollenteur.nl>
2016-04-20 13:16:01 +0200

    Added docstrings in tseries.py
    Docstrings are added in tseries.py. At the top of the page the minimum
    requirements for each tseries class is defined.
    
    The questions is whether we want to provide a elaborate docstring for
    each tseries class (E.g. Tseries) or a short summary of each tseries
    class (E.g. Tseries2).

--- gwtsa/tseries.py
+++ gwtsa/tseries.py
@@ -2,12 +2,38 @@ import numpy as np
 import pandas as pd
 from scipy.signal import fftconvolve
 
-'''Need a description of the requirements of all the timeseries'''
+"""The tseries.py file contains all time series objects.
+
+Each class defined in tseries should at least have the following:
+
+Attributes
+----------
+stress : Pandas Series
+    Series object of the stress with index in datetime format.
+rfunc : Object
+    Response function defined in rfunc.py, E.g. Gamma().
+npoints : int
+    length of the stress Series.
+nparam : int
+    Number of parameters for this tseries object. Depending on
+    rfunc.nparam plus the parameters in used in this class.
+name : str
+    Name of this tseries object. Used as prefix for the parameters.
+parameters : Pandas Dataframe
+    Dataframe containing the parameters.
+
+Methods
+-------
+simulate
+
+"""
 
 
 class Tseries:
-    '''Time series model consisting of the convolution of one stress with one
-    response function'''
+    """Time series model consisting of the convolution of one stress with one
+    response function.
+
+    """
     def __init__(self, stress, rfunc, name):
         self.stress = stress
         self.rfunc = rfunc
@@ -17,6 +43,22 @@ class Tseries:
         self.parameters = self.rfunc.set_parameters(self.name)
 
     def simulate(self, tindex=None, p=None):
+        """ Simulates the head contribution.
+
+        Parameters
+        ----------
+        tindex:
+            Time indices to simulate the model.
+        p: Optional[array-like]
+            Parameters used for simulation. If p is not
+            provided, parameters attribute will be used.
+
+        Returns
+        -------
+        Pandas Series Object
+            The simulated head contribution.
+
+        """
         if p is None: p = np.array(self.parameters.value)
         b = self.rfunc.block(p)
         h = pd.Series(fftconvolve(self.stress, b, 'full')[:self.npoints],
@@ -27,10 +69,14 @@ class Tseries:
 
 
 class Tseries2:
-    '''Time series model consisting of the convolution of two stresses with
-    one response function
+    """Time series model consisting of the convolution of two stresses with
+    one response function.
+
     stress = stress1 + factor * stress2
-    Last parameters is factor'''
+
+    Last parameters is factor
+
+    """
     def __init__(self, stress1, stress2, rfunc, name):
         self.stress1 = stress1
         self.stress2 = stress2
@@ -54,7 +100,15 @@ class Tseries2:
 
 
 class Constant:
-    '''A constant value'''
+    """A constant value.
+
+    Parameters
+    ----------
+    value : Optional[float]
+        Initial estimate of the parameter value. E.g. The minimum of the observed
+        series.
+
+    """
     def __init__(self, value=0.0):
         self.nparam = 1
         self.parameters = pd.DataFrame(columns=['value', 'pmin', 'pmax', 'vary'])
@@ -66,13 +120,46 @@ class Constant:
 
 
 class NoiseModel:
+    """Noise model with exponential decay of the residual.
+
+    Notes
+    -----
+    Calculates the innovations [1] according to:
+    v(t1) = r(t1) - r(t0) * exp(- (t1 - t0) / alpha)
+
+    References
+    ----------
+    .. [1] von Asmuth, J. R., and M. F. P. Bierkens (2005), Modeling irregularly
+    spaced residual series as a continuous stochastic process, Water Resour.
+    Res., 41, W12404, doi:10.1029/2004WR003726.
+
+    """
     def __init__(self):
         self.nparam = 1
         self.parameters = pd.DataFrame(columns=['value', 'pmin', 'pmax', 'vary'])
         self.parameters.loc['noise_alpha'] = (14.0, 0, 5000, 1)
 
     def simulate(self, res, delt, tindex=None, p=None):
+        """
+
+        Parameters
+        ----------
+        res : Pandas Series
+            The residual series.
+        delt : Pandas Series
+            Time steps between observations.
+        tindex : Optional[None]
+            Time indices used for simulation.
+        p : Optional[array-like]
+            Alpha parameters used by the noisemodel.
+
+        Returns
+        -------
+        Pandas Series
+            Series of the innovations.
+        """
         if p is None: p = np.array(self.parameters.value)
         innovations = pd.Series(res, index=res.index)
-        innovations[1:] -= np.exp(-delt[1:] / p) * res.values[:-1]  # values is needed else it gets messed up with the dates
+        innovations[1:] -= np.exp(-delt[1:] / p) * res.values[:-1]  # values is
+        # needed else it gets messed up with the dates
         return innovations
