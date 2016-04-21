ec81d2c0bba0e193c8e87498b0291ee793c3d6ea
Mark Bakker <markbak@gmail.com>
2016-04-21 09:11:46 +0200

    docs string of rfunc

--- gwtsa/rfunc.py
+++ gwtsa/rfunc.py
@@ -3,32 +3,34 @@ import pandas as pd
 from scipy.special import gammainc, gammaincinv
 
 '''
-rfunc.py contains the response functions.
-
-Each response function is stored as a class and should at least contain the 
-following:
+rfunc module.
+Contains classes for the response functions.
+Each response function class needs the following:
 
 Attributes
 ----------
 nparam: integer
     number of parameters.
 cutoff: float
-    percentage after which the response is cutoff.
+    percentage after which the step function is cut off.
 
 Functions
 ---------
-__init__(self)
-    function that initialized an instance of the response function.
 set_parameters(self, name)
-    This function is called by an tseries object. Name is used as a prefix
-    for the parameter names. A Pandas dataframe is returned that contains
-    the values, minimum, maximum, and vary for each parameter.
+    A function that returs a Pandas DataFrame of the parameters of the
+    response function. Columns of the dataframe need to be
+    ['value', 'pmin', 'pmax', 'vary'].
+    Rows of the DataFrame have the for name_ followed by the symbol
+    for the parameter.
+    This function is called by a Tseries object.
 step(self, p)
-    This function calculates and returns an array of the step response. Input
-    p is an array of parameters in a specific order.
+    Returns an array of the step response. Input
+    p is a numpy array of parameter values in the same order as
+    defined in set_parameters.
 block(self, p)
-    This function calls the step function and returns and array of the block
-    response. Input p is an array of parameters in a specific order.
+    Returns an array of the block response. Input
+    p is a numpy array of parameter values in the same order as
+    defined in set_parameters.
 
 More information on how to write a response class can be found here:
 https://github.com/gwtsa/gwtsa/wiki
@@ -38,6 +40,9 @@ gwtsa -2016
 
 
 class Gamma:
+    '''
+    step(t) = A * Gammainc(n, t / a)
+    '''
     def __init__(self):
         self.nparam = 3
         self.cutoff = 0.99
@@ -61,6 +66,10 @@ class Gamma:
 
 
 class Exponential:
+    '''
+    step(t) = A * (1 - exp(-t / a))
+    '''
+    
     def __init__(self):
         self.nparam = 2
         self.cutoff = 0.99
@@ -74,7 +83,7 @@ class Exponential:
     def step(self, p):
         self.tmax = -np.log(1.0 / p[1]) * p[1]
         t = np.arange(1.0, self.tmax)
-        s = -p[0] * np.exp(- t / p[1]) + p[0]
+        s = p[0] * (1.0 - np.exp(-t / p[1]))
         return s
 
     def block(self, p):
