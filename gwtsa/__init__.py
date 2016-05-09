a8d35a42a0a53bee152d0cd683f1e5c00069e13e
raoulcollenteur <info@raoulcollenteur.nl>
2016-05-09 17:24:21 +0200

    Added TseriesWell class and two response functions
    - Added a TseriesWell class to simulate the effect of wells. Allows for
    multiple wells, as the distance from the point of the observations well
    needs to be provided.
    
    - Added a Theis well function
    
    - Added a Hantush well function

--- gwtsa/__init__.py
+++ gwtsa/__init__.py
@@ -1,9 +1,9 @@
-__name__='gwtsa'
-__author__='gwtsa team'
+__name__ = 'gwtsa'
+__author__ = 'gwtsa team'
 __docformat__ = 'NumPy'
 from version import __version__
 
 from model import Model
-from rfunc import Gamma, Exponential
-from tseries import Tseries, Tseries2, Constant, NoiseModel
+from rfunc import Gamma, Exponential, Hantush, Theis
+from tseries import Tseries, Tseries2, TseriesWell, Constant, NoiseModel
 from stats import Statistics
