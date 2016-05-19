21596ab96b6faea931328b81dee77409c59d6687
raoulcollenteur <info@raoulcollenteur.nl>
2016-05-19 18:44:58 +0200

    Remove version from init

--- gwtsa/__init__.py
+++ gwtsa/__init__.py
@@ -1,7 +1,6 @@
 __name__ = 'gwtsa'
 __author__ = 'gwtsa team'
 __docformat__ = 'NumPy'
-from version import __version__
 
 from model import Model
 from rfunc import Gamma, Exponential, Hantush, Theis
