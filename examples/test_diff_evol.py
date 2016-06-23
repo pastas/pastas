06e5ab3e5c02a230dfcc1d5970d0a8a01946ca74
Mark Bakker <markbak@gmail.com>
2016-06-23 13:38:16 +0200

    changes to pysot and new test case for differential evolution

--- examples/test_diff_evol.py
+++ examples/test_diff_evol.py
@@ -37,6 +37,7 @@ ml.addnoisemodel(n)
 # Solve model to get good starting value
 ml.solve()
 
+import numpy as np
 
 class PastaTest:
     def __init__(self, model):
@@ -53,7 +54,7 @@ class PastaTest:
             self.xup.extend(ml.noisemodel.parameters['pmax'].tolist())
         self.xlow = np.array(self.xlow)
         self.xup = np.array(self.xup)
-        #
+        # option 2
         #faclow = 0.5 * np.ones(self.dim)
         #facup = 2 * np.ones(self.dim)
         #faclow[ml.parameters < 0] = 2
@@ -68,44 +69,7 @@ class PastaTest:
         print '.',
         return self.model.sse(x, noise=True)
     
-# Import the necessary modules
-from pySOT import *
-from poap.controller import SerialController, BasicWorkerThread
-import numpy as np
-
-# Decide how many evaluations we are allowed to use
-maxeval = 800
-
-# (1) Optimization problem
 data = PastaTest(ml)
-
-# (2) Experimental design
-# Use a symmetric Latin hypercube with 2d + 1 samples
-exp_des = SymmetricLatinHypercube(dim=data.dim, npts=2 * data.dim + 1)
-
-# (3) Surrogate model
-# Use a cubic RBF interpolant, with the domain scaled to the unit box
-surrogate = RSUnitbox(RBFInterpolant(surftype=CubicRBFSurface, maxp=maxeval), data)
-
-# (4) Adaptive sampling
-# Use DYCORS with 100d candidate points
-adapt_samp = CandidateDYCORS(data=data, numcand=100 * data.dim)
-
-# Use the serial controller (uses only one thread)
-controller = SerialController(data.objfunction)
-
-# (5) Use the sychronous strategy without non-bound constraints
-strategy = SyncStrategyNoConstraints(
-        worker_id=0, data=data, maxeval=maxeval, nsamples=1,
-        exp_design=exp_des, response_surface=surrogate, 
-        sampling_method=adapt_samp)
-controller.strategy = strategy
-
-# Run the optimization strategy
-result = controller.run()
-
-# Print the final result
-print('Best value found: {0}'.format(result.value))
-print('Best solution found: {0}'.format(
-    np.array_str(result.params[0], max_line_width=np.inf,
-                precision=5, suppress_small=True)))
+    
+from scipy.optimize import differential_evolution
+result = differential_evolution(data.objfunction, zip(data.xlow, data.xup))
