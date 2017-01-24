9eabdbd9463b1bfa2a45d976427bca9c7746e4a9
Ruben Caljé <r.calje@artesia-water.nl>
2017-01-24 14:57:21 +0100

    Update Menyanthes-example

--- examples/example_menyanthes.py
+++ examples/example_menyanthes.py
@@ -5,15 +5,9 @@ test the functioning of Pastas during development.
 
 from pastas import *
 from pastas.read.menydata import MenyData
-import matplotlib.pyplot as plt
-import matplotlib.ticker as plticker
 
 fname = 'data/MenyanthesTest.men'
 meny = MenyData(fname)
-
-# Round the time of observations to days (using ceil, so the observation is
-# never before the stress)
-#meny.H[0].series.index = meny.H[0].series.index.ceil('d')
 
 # Create the time series model
 ml = Model(meny.H[0].series)
@@ -22,35 +16,36 @@ ml = Model(meny.H[0].series)
 IN = next(x for x in meny.IN if x.name == 'Precipitation')
 # round to days (precipitation is measured at 9:00)
 IN.series = IN.series.resample('d').bfill()
-if False:
-    ts = Tseries(IN.series, Gamma, IN.name)
-else:
-    IN2 = next(x for x in meny.IN if x.name == 'Evaporation')
-    IN2.series = IN2.series.resample('d').bfill()
-    ts = Tseries2(IN.series, IN2.series, Gamma, 'Recharge')
+IN2 = next(x for x in meny.IN if x.name == 'Evaporation')
+# round to days (evaporation is measured at 1:00)
+IN2.series = IN2.series.resample('d').bfill()
+ts = Tseries2(IN.series, IN2.series, Gamma, 'Recharge')
 ml.add_tseries(ts)
 
-# Add well extraction at Ossendrecht
+# Add well extraction 1
 IN = next(x for x in meny.IN if x.name == 'Extraction 1')
 # extraction amount counts for the previous month
 IN.series = IN.series.resample('d').bfill()
 IN.name = IN.name.replace(' ','_')
+# divide by thousand, as default starting parameters for gamma are wrong
 ts = Tseries(-IN.series/1000, Gamma, IN.name)
 ml.add_tseries(ts)
 
-# Add well extraction at Huijbergen
+# Add well extraction 2
 IN = next(x for x in meny.IN if x.name == 'Extraction 2')
 # extraction amount counts for the previous month
 IN.series = IN.series.resample('d').bfill()
 IN.name = IN.name.replace(' ','_')
+# divide by thousand, as default starting parameters for gamma are wrong
 ts = Tseries(-IN.series/1000, Gamma, IN.name)
 ml.add_tseries(ts)
 
-# Add well extraction at Essen
+# Add well extraction 3
 IN = next(x for x in meny.IN if x.name == 'Extraction 3')
 # extraction amount counts for the previous month
 IN.series = IN.series.resample('d').bfill()
 IN.name = IN.name.replace(' ','_')
+# divide by thousand, as default starting parameters for gamma are wrong
 ts = Tseries(-IN.series/1000, Gamma, IN.name)
 ml.add_tseries(ts)
 
