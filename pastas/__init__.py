6e9b11975bc581acd942f735ee10697ecfbdf198
Raoul Collenteur <info@raoulcollenteur.nl>
2017-02-13 16:41:27 +0100

    Include version in package

--- pastas/__init__.py
+++ pastas/__init__.py
@@ -1,17 +1,11 @@
 from __future__ import print_function
 
-# Get the version.
-# version = {}
-# with open("version.py") as fp:
-#     exec(fp.read(), version)
-# __version__ = version['__version__']
-# del version
-
-from .model import Model
 from . import read
+from .model import Model
 from .recharge.recharge_func import Preferential, Linear, Percolation, \
     Combination
 from .rfunc import Gamma, Exponential, Hantush, Theis
 from .solver import LmfitSolve, DESolve
 from .stats import Statistics
 from .tseries import Tseries, Tseries2, Recharge, Well, Constant, NoiseModel
+from .version import __version__
