1405f2eb2332fa9d0f5bca048db351ab21109491
Raoul Collenteur <info@raoulcollenteur.nl>
2017-02-14 17:30:29 +0100

    Updated stats module docs
    -Updated the stats module documentation
    - Updated the documentation website

--- pastas/stats.py
+++ pastas/stats.py
@@ -37,8 +37,8 @@ from statsmodels.tsa.stattools import acf, pacf
 
 class Statistics(object):
     def __init__(self, ml):
-        """This class is inherited by the Model class of Pastas after a
-        model is calibrated. To obtain a list of all statistics that are
+        """
+        To obtain a list of all statistics that are
         included type:
 
         >>> print(ml.stats.ops)
@@ -55,16 +55,28 @@ class Statistics(object):
                     'rmsi': 'Root mean squared innovation',
                     'sse': 'Sum of squares of the error',
                     'avg_dev': 'Average Deviation',
-                    'corr': 'Pearson R^2',
-                    'corr_corrected': 'Corrected Pearson R^2',
+                    'rsq': 'Pearson R^2',
+                    'rsq_adj': 'Adjusted Pearson R^2',
                     'bic': 'Bayesian Information Criterion',
                     'aic': 'Akaike Information Criterion'}
+
+    def __repr__(self):
+        msg = """This module contains all the statistical functions that are
+included in Pastas. To obtain a list of all statistics that are included type:
+
+    >>> print(ml.stats.ops)"""
+        return msg
 
     # The statistical functions
 
     def rmse(self, tmin=None, tmax=None):
         """Root mean squared error of the residuals.
-        rmse = sqrt(sum(residuals**2) / N)
+
+        Notes
+        -----
+        .. math:: rmse = sqrt(sum(residuals**2) / N)
+
+        where N is the number of residuals.
         """
         res = self.ml.get_residuals(tmin, tmax)
         N = res.size
@@ -72,44 +84,92 @@ class Statistics(object):
 
     def rmsi(self, tmin=None, tmax=None):
         """Root mean squared error of the innovations.
-        rmsi = sqrt(sum(innovations**2) / N)
+
+        Notes
+        -----
+        .. math:: rmsi = sqrt(sum(innovations**2) / N)
+
+        where N is the number of innovations.
         """
         res = self.ml.get_innovations(tmin, tmax)
         N = res.size
         return np.sqrt(sum(res ** 2) / N)
 
     def sse(self, tmin=None, tmax=None):
+        """Sum of the squares of the error (SSE)
+
+        Notes
+        -----
+        The SSE is calculated as follows:
+
+        .. math:: SSE = sum(E ** 2)
+
+        Where E is an array of the residual series.
+
+        """
         res = self.ml.get_residuals(tmin, tmax)
         return sum(res ** 2)
 
     def avg_dev(self, tmin=None, tmax=None):
         """Average deviation of the residuals.
+
+        Notes
+        -----
+        .. math:: avg_dev = sum(E) / N
+
+        Where N is the number of the residuals.
+
         """
         res = self.ml.get_residuals(tmin, tmax)
         return res.mean()
 
     def evp(self, tmin=None, tmax=None):
         """Explained variance percentage.
-        evp = (var(h) - var(res)) / var(h) * 100%
-        """
 
+        Notes
+        -----
+        Commonly used statistic in time series models of groundwater level.
+        It has to be noted that a high EVP value does not necessarily indicate
+        a good time series model.
+
+        .. math:: evp = (var(h) - var(res)) / var(h) * 100%
+
+        """
         res = self.ml.get_residuals(tmin, tmax)
         obs = self.ml.get_observations(tmin, tmax)
         return (np.var(obs) - np.var(res)) / np.var(obs) * 100.0
 
-    def corr(self, tmin=None, tmax=None):
+    def rsq(self, tmin=None, tmax=None):
         """Correlation between observed and simulated series.
-        """
 
+        Notes
+        -----
+        For the calculation of this statistic the corrcoef method from numpy
+        is used.
+
+        >>> np.corrcoef(sim, obs)[0, 1]
+
+        Please refer to the Numpy Docs:
+        https://docs.scipy.org/doc/numpy/reference/generated/numpy.corrcoef.html#numpy.corrcoef
+
+        """
         sim = self.ml.get_simulation(tmin, tmax)
         obs = self.ml.get_observations(tmin, tmax)
         sim = sim[obs.index]  # Make sure to correlate the same in time.
         return np.corrcoef(sim, obs)[0, 1]
 
-    def corr_corrected(self, tmin=None, tmax=None):
-        """Corrected R-squared.
+    def rsq_adj(self, tmin=None, tmax=None):
+        """R-squared Adjusted for the number of free parameters.
 
-        R_corrected = 1 - (n-1) / (n-N_param) * RSS/TSS
+        Notes
+        -----
+        .. math:: R_{corrected} = 1 - (n-1) / (n-N_param) * RSS/TSS
+
+        Where:
+            RSS = sum of the squared residuals.
+            TSS = ??
+            N = Number of observations
+            N_Param = Number of free parameters
         """
 
         obs = self.ml.get_observations(tmin, tmax)
@@ -122,8 +182,15 @@ class Statistics(object):
 
     def bic(self, tmin=None, tmax=None):
         """Bayesian Information Criterium.
-        BIC = -2 log(L) + nparam * log(N)
-        nparam : Number of free parameters
+
+        Notes
+        -----
+        The Bayesian Information Criterium is calculated as follows:
+
+        .. math:: BIC = -2 log(L) + nparam * log(N)
+
+        Where:
+            nparam : Number of free parameters
         """
         innovations = self.ml.get_innovations(tmin, tmax)
         n = innovations.size
@@ -132,10 +199,15 @@ class Statistics(object):
         return bic
 
     def aic(self, tmin=None, tmax=None):
-        """Akaike Information Criterium.
-        AIC = -2 log(L) + 2 nparam
-        nparam : Number of free parameters
-        L: likelihood function for the model.
+        """Akaike Information Criterium (AIC).
+
+        Notes
+        -----
+        .. math:: AIC = -2 log(L) + 2 nparam
+
+        Where
+            nparam = Number of free parameters
+            L = likelihood function for the model.
         """
         innovations = self.ml.get_innovations(tmin, tmax)
         nparam = len(self.ml.parameters[self.ml.parameters.vary == True])
@@ -144,20 +216,35 @@ class Statistics(object):
 
     def acf(self, tmin=None, tmax=None, nlags=20):
         """Autocorrelation function.
-        TODO
-        ----
-        Compute autocorrelation for irregulat time steps.
+
+        Notes
+        -----
+        For the autocorrelation function the acf method from Statsmodels
+        package is used untill an alternative is found. However, please be
+        aware that this can lead to incorrect values for irregular time steps.
+
+        Please refer to the Statsmodels docs:
+        http://statsmodels.sourceforge.net/devel/_modules/statsmodels/tsa/stattools.html#acf
+
+        TODO: Compute autocorrelation for irregulat time steps.
         """
         innovations = self.ml.get_innovations(tmin, tmax)
         return acf(innovations, nlags=nlags)
 
     def pacf(self, tmin=None, tmax=None, nlags=20):
         """Partial autocorrelation function.
-        http://statsmodels.sourceforge.net/devel/_modules/statsmodels/tsa/stattools.html#acf
 
-        TODO
-        ----
-        Compute  partial autocorrelation for irregulat time steps.
+        Notes
+        -----
+        For the partial autocorrelation function the acf method from
+        Statsmodels package is used untill an alternative is found. However,
+        please be aware that this can lead to incorrect values for irregular
+        time steps.
+
+        Please refer to the Statsmodels docs:
+        http://statsmodels.sourceforge.net/devel/_modules/statsmodels/tsa/stattools.html#pacf
+
+        TODO: Compute  partial autocorrelation for irregulat time steps.
         """
         innovations = self.ml.get_innovations(tmin, tmax)
         return pacf(innovations, nlags=nlags)
@@ -228,8 +315,14 @@ class Statistics(object):
     #         return np.mean(np.array(x))
 
     def descriptive(self, tmin=None, tmax=None):
-        series = self.__getallseries__(tmin, tmax)
-        series.describe()
+        """Returns the descriptive statistics for all time series.
+
+        """
+        return print("This method is currently not supported. Please use:"
+                     ">>> ml.get_simulations().describe() to make use of the "
+                     "built-in Pandas methods.")
+        #series = self.__getallseries__(tmin, tmax)
+        #series.describe()
 
     def plot_diagnostics(self, tmin=None, tmax=None):
         innovations = self.ml.get_innovations(tmin, tmax)
