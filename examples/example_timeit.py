d3f9dcc3ae6b46a27530cde7241dde6cfd7f4dc0
tomvansteijn <tom.van.steijn@rhdhv.com>
2017-02-27 23:35:33 +0100

    added faster test example using pickle

--- examples/example_timeit.py
+++ examples/example_timeit.py
@@ -7,21 +7,27 @@ test the functioning of PASTA during development.
 # timeit command: python -m timeit -n 3 -r 1 -c "import example_timeit; example_timeit.main()"
 from pastas import *
 
+import pandas as pd
+
 def main():
     # read observations
-    obs = read.dinodata('data/B58C0698001_1.csv')
+    # obs = read.dinodata('data/B58C0698001_1.csv')
+    # obs.series.to_pickle('data/obs.pickle')
 
     # Create the time series model
-    ml = Model(obs.series)
+    ml = Model(pd.read_pickle('data/obs.pickle'))
 
-    # read weather data
-    rain = read.knmidata('data/neerslaggeg_HEIBLOEM-L_967-2.txt', variable='RD')
-    #from pandas import read_csv
-    #rain = read_csv('data/Heibloem_rain_data.dat', skiprows=4, sep=' ', skipinitialspace=True, parse_dates='date', index_col='date')
-    evap = read.knmidata('data/etmgeg_380.txt', variable='EV24')
+    # # read weather data
+    # rain = read.knmidata('data/neerslaggeg_HEIBLOEM-L_967-2.txt', variable='RD')
+    # #from pandas import read_csv
+    # #rain = read_csv('data/Heibloem_rain_data.dat', skiprows=4, sep=' ', skipinitialspace=True, parse_dates='date', index_col='date')
+    # evap = read.knmidata('data/etmgeg_380.txt', variable='EV24')
+
+    # rain.series.to_pickle('data/rain.pickle')
+    # evap.series[1965:].to_pickle('data/evap.pickle')
 
     ## Create stress
-    ts = Tseries2(rain.series, evap.series[1965:], Gamma, name='recharge')
+    ts = Tseries2(pd.read_pickle('data/rain.pickle'), pd.read_pickle('data/evap.pickle'), Gamma, name='recharge')
     #ts = Tseries2(rain.precip * 0.001, evap.series[1965:], Gamma, name='recharge')
     ml.add_tseries(ts)
 
