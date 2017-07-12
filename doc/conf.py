a7e801caf6a306383804cb646c2cebe1e6cdd88f
Raoul Collenteur <info@raoulcollenteur.nl>
2017-07-12 19:47:01 +0200

    Make ipython code beautiful

--- doc/conf.py
+++ doc/conf.py
@@ -39,6 +39,7 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'IPython.sphinxext.ipython_console_highlighting',# lowercase didn't work
     'nbsphinx'
 ]
 
