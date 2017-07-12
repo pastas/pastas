e3d4ad935bbe03c0db0ec10652eb55a0eaa50640
Raoul Collenteur <info@raoulcollenteur.nl>
2017-07-12 19:32:46 +0200

    Fix readthedocs build

--- doc/conf.py
+++ doc/conf.py
@@ -39,7 +39,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'IPython.sphinxext.ipython_console_highlighting',# lowercase didn't work
     'nbsphinx'
 ]
 
