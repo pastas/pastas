1fa2b0905fc8234b54b235ccf6741033e256fe3a
Raoul Collenteur <info@raoulcollenteur.nl>
2017-10-24 20:07:23 +0200

    Cleaned up the test directory
    And added test_004 to work.

--- tests/test_002.py
+++ tests/test_002.py
@@ -1,41 +1,22 @@
-import pandas as pd
-from pastas import *
+import pastas as ps
 
 
 def test_model():
     # Import and check the observed groundwater time series
-    gwdata = pd.read_csv('tests/data/B58C0698001_0.csv', skiprows=11,
-                         parse_dates=['PEIL DATUM TIJD'],
-                         index_col='PEIL DATUM TIJD',
-                         skipinitialspace=True)
-    gwdata.rename(columns={'STAND (MV)': 'h'}, inplace=True)
-    gwdata.index.names = ['date']
-    gwdata.h *= 0.01
-    oseries = 30.17 - gwdata.h  # NAP
-
-    # Import and check the observed precipitation series
-    rain = pd.read_csv('tests/data/Heibloem_rain_data.dat', skiprows=4,
-                       delim_whitespace=True,
-                       parse_dates=['date'],
-                       index_col='date')
-    rain = rain.precip
-    rain /= 1000.0  # Meters
-
-    # Import and check the observed evaporation series
-    evap = pd.read_csv('tests/data/Maastricht_E_June2015.csv', skiprows=4,
-                       sep=';',
-                       parse_dates=['DATE'],
-                       index_col='DATE')
-    evap.rename(columns={'VALUE (m-ref)': 'evap'}, inplace=True)
-    evap = evap.evap
+    obs = ps.read_dino('tests/data/dino_gwl_data.csv')
 
     # Create the time series model
-    ml = Model(oseries)
+    ml = ps.Model(obs, name="Test_Model")
 
-    ts1 = StressModel2(stress=[rain, evap], rfunc=Gamma, name='recharge')
-    ml.add_stressmodel(ts1)
-    n = NoiseModel()
-    ml.add_noisemodel(n)
+    # read weather data
+    rain = ps.read_knmi('tests/data/knmi_rain_data.txt',
+                        variables='RD')
+    evap = ps.read_knmi('tests/data/knmi_evap_data.txt', variables='EV24')
+
+    ## Create stress
+    sm = ps.StressModel2(stress=[rain, evap], rfunc=ps.Exponential,
+                         name='recharge')
+    ml.add_stressmodel(sm)
 
     # Solve the time series model
     ml.solve()
--- tests/test_004.py
+++ tests/test_004.py
@@ -1,16 +1,15 @@
 import pastas as ps
 
-def create_model():
+def test_create_model():
     # Import and check the observed groundwater time series
-    obs = ps.read_dino('data/B58C0698001_1.csv')
+    obs = ps.read_dino('tests/data/dino_gwl_data.csv')
 
     # Create the time series model
     ml = ps.Model(obs, name="Test_Model")
 
     # read weather data
-    rain = ps.read_knmi('data/neerslaggeg_HEIBLOEM-L_967-2.txt',
-                        variables='RD')
-    evap = ps.read_knmi('data/etmgeg_380.txt', variables='EV24')
+    rain = ps.read_knmi('tests/data/knmi_rain_data.txt', variables='RD')
+    evap = ps.read_knmi('tests/data/knmi_evap_data.txt', variables='EV24')
 
     ## Create stress
     sm = ps.StressModel2(stress=[rain, evap], rfunc=ps.Exponential,
@@ -19,17 +18,16 @@ def create_model():
 
     ## Solve
     ml.solve()
-    ml.plot()
 
     return ml
 
-def save_model():
-    ml = create_model()
+def test_save_model():
+    ml = test_create_model()
     ml.dump("test.pas")
 
     return
 
-def load_model():
-    save_model()
+def test_load_model():
+    test_save_model()
     ml = ps.io.load("test.pas")
     return
