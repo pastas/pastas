aa19e113e6dd413e4d04192f69ba2ffd72cabdfa
Raoul Collenteur <info@raoulcollenteur.nl>
2018-02-05 16:54:42 +0100

    Undo accidental commit of example.py

--- examples/example.py
+++ examples/example.py
@@ -13,14 +13,13 @@ ml = ps.Model(obs)
 
 # read weather data
 rain = ps.read_knmi('data/neerslaggeg_HEIBLOEM-L_967-2.txt', variables='RD')
-#rain.update_series(norm="mean")
 evap = ps.read_knmi('data/etmgeg_380.txt', variables='EV24')
-#evap.update_series(norm="mean")
-## Create stress
+
+# Create stress
 sm = ps.StressModel2(stress=[rain, evap], rfunc=ps.Exponential,
                      name='recharge')
 ml.add_stressmodel(sm)
 
 ## Solve
-ml.solve(fit_constant=False)
+ml.solve()
 ml.plot()
