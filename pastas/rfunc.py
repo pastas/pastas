Big fix in tmax calculation of exponential response
Tmax was not calculated correctly. This calculation could also work for the Gamma response and is actually about 2 times faster..

pastas/rfunc.py
@@ -126,7 +126,7 @@ class Exponential(RfuncBase):
         return parameters
 
     def calc_tmax(self, p):
-        return -np.log(1.0 / p[1]) * p[1]
+        return -p[1] * np.log(1 - self.cutoff)
 
     def gain(self, p):
         return self.up * p[0]