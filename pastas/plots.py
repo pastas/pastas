45a82eb113ab56e803a2977f7c3f5eecb987c381
Raoul Collenteur <raoulcollenteur@gmail.com>
2018-09-26 11:08:50 +0200

    Cleanup of plots.py

--- pastas/plots.py
+++ pastas/plots.py
@@ -15,14 +15,18 @@ from scipy.stats import probplot
 from .decorators import model_tmin_tmax
 from .stats import acf
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class Plotting:
     def __init__(self, ml):
         self.ml = ml  # Store a reference to the model class
 
     def __repr__(self):
-        msg = "This module contains all the built-in plotting options that are " \
-              "available."
+        msg = "This module contains all the built-in plotting options that " \
+              "are available."
         return msg
 
     @model_tmin_tmax
@@ -32,37 +36,40 @@ class Plotting:
 
         Parameters
         ----------
-        oseries: Boolean
+        tmin: str or pandas.Timestamp, optional
+        tmax: str or pandas.Timestamp, optional
+        oseries: bool, optional
             True to plot the observed time series.
-        simulation: Boolean
+        simulation: bool, optional
             True to plot the simulated time series.
 
         Returns
         -------
         ax: matplotlib.axes
-            matplotlib axes with the simulated and optionally the observed time series.
+            matplotlib axes with the simulated and optionally the observed
+            timeseries.
 
         """
-        fig = self._get_figure(**kwargs)
-        fig.suptitle("Results of " + self.ml.name)
+        ax = plt.subplot(**kwargs)
+        ax.set_title("Results of {}".format(self.ml.name))
 
         if oseries:
             o = self.ml.observations(tmin=tmin, tmax=tmax)
             o_nu = self.ml.oseries.series.drop(o.index)
             if not o_nu.empty:
                 # plot parts of the oseries that are not used in grey
-                o_nu.plot(linestyle='', marker='.', color='0.5', fig=fig,
-                          label='')
-            o.plot(linestyle='', marker='.', color='k', fig=fig)
+                o_nu.plot(linestyle='', marker='.', color='0.5', label='',
+                          ax=ax)
+            o.plot(linestyle='', marker='.', color='k', ax=ax)
 
         if simulation:
             sim = self.ml.simulate(tmin=tmin, tmax=tmax)
-            sim.plot(fig=fig)
+            sim.plot(ax=ax)
         plt.xlim(tmin, tmax)
         plt.ylabel("Groundwater levels [meter]")
         plt.legend()
-
-        return fig.axes
+        plt.tight_layout()
+        return ax
 
     @model_tmin_tmax
     def results(self, tmin=None, tmax=None, figsize=(10, 8), **kwargs):
@@ -70,19 +77,19 @@ class Plotting:
 
         Parameters
         ----------
-        tmin/tmax: str
-            start and end time for plotting
-
+        tmin: str or pandas.Timestamp, optional
+        tmax: str or pandas.Timestamp, optional
+        figsize: tuple, optional
+            tuple of size 2 to determine the figure size in inches.
 
         Returns
         -------
+        matplotlib.axes
 
         """
-        fig = self._get_figure(figsize=figsize, **kwargs)
-
         # Number of rows to make the figure with
         rows = 3 + len(self.ml.stressmodels)
-
+        fig = plt.figure(figsize=figsize, **kwargs)
         # Main frame
         ax1 = plt.subplot2grid((rows, 3), (0, 0), colspan=2, rowspan=2)
         o = self.ml.observations(tmin=tmin, tmax=tmax)
@@ -131,7 +138,7 @@ class Plotting:
 
         ax1.set_xlim(tmin, tmax)
 
-        fig.tight_layout(pad=0.0)
+        plt.tight_layout(pad=0.0)
 
         # Draw parameters table
         parameters = self.ml.parameters.copy()
@@ -282,19 +289,30 @@ class Plotting:
 
     @model_tmin_tmax
     def diagnostics(self, tmin=None, tmax=None):
+        """Plot a window that helps in diagnosing basic model assumptions.
+
+        Parameters
+        ----------
+        tmin
+        tmax
+
+        Returns
+        -------
+        matplotlib.axes
+
+        """
         if self.ml.settings["noise"]:
             res = self.ml.noise(tmin=tmin, tmax=tmax)
         else:
             res = self.ml.residuals(tmin=tmin, tmax=tmax)
 
-        fig = self._get_figure()
         shape = (2, 3)
         ax = plt.subplot2grid(shape, (0, 0), colspan=2, rowspan=1)
         ax.set_title(res.name)
         res.plot(ax=ax)
 
         ax1 = plt.subplot2grid(shape, (1, 0), colspan=2, rowspan=1)
-        ax1.set_title('Autocorrelation')
+        ax1.set_ylabel('Autocorrelation')
         conf = 1.96 / np.sqrt(res.index.size)
         r = acf(res)
 
@@ -313,90 +331,80 @@ class Plotting:
         c = ax.get_lines()[0]._color
         ax3.get_lines()[0].set_color(c)
 
-        plt.tight_layout()
-        return fig.axes
+        plt.tight_layout(pad=0.0)
+        return plt.gca()
 
-    def block_response(self, series=None):
-        """Plot the block response for a specific series.
+    def block_response(self, stressmodels=None, **kwargs):
+        """Plot the block response for a specific stressmodels.
 
-        Returns
-        -------
-        fig: matplotlib.axes
-            matplotlib axes instance.
-
-        """
-        if not series:
-            series = self.ml.stressmodels.keys()
-        else:
-            series = [series]
-
-        legend = []
-        fig = self._get_figure()
-
-        for name in series:
-            if name not in self.ml.stressmodels.keys():
-                return None
-            elif hasattr(self.ml.stressmodels[name], 'rfunc'):
-                plt.plot(self.ml.get_block_response(name))
-                legend.append(name)
-            else:
-                pass
-
-        plt.xlim(0)
-
-        # Change xtickers to the correct time
-        # locs, labels = plt.xticks()
-        # labels = locs * get_dt(self.ml.settings["freq"])
-        # plt.xticks(locs, labels)
-        plt.xlabel("Time [days]")
-
-        plt.legend(legend)
-        fig.suptitle("Block Response(s)")
-
-        return fig.axes
-
-    def step_response(self, series=None):
-        """Plot the step response for a specific series.
+        Parameters
+        ----------
+        stressmodels: list, optional
+            List with the stressmodels to plot the block response for.
 
         Returns
         -------
-        axes: matplotlib.axes
+        matplotlib.axes
             matplotlib axes instance.
 
         """
-        if not series:
-            series = self.ml.stressmodels.keys()
-        else:
-            series = [series]
+        if not stressmodels:
+            stressmodels = self.ml.stressmodels.keys()
 
         legend = []
-        fig = self._get_figure()
 
-        for name in series:
-            if name not in self.ml.stressmodels.keys():
-                return None
-            elif hasattr(self.ml.stressmodels[name], 'rfunc'):
-                plt.plot(self.ml.get_step_response(name))
+        ax = plt.subplot(**kwargs)
+
+        for name in stressmodels:
+            if hasattr(self.ml.stressmodels[name], 'rfunc'):
+                self.ml.get_block_response(name).plot(ax=ax)
                 legend.append(name)
             else:
-                pass
+                logger.warning("Stressmodel {} not in stressmodels "
+                               "list.".format(name))
 
         plt.xlim(0)
-
-        # Change xtickers to the correct time
-        # locs, labels = plt.xticks()
-        # labels = locs * get_dt(self.ml.settings["freq"])
-        # plt.xticks(locs, labels)
         plt.xlabel("Time [days]")
-
         plt.legend(legend)
-        fig.suptitle("Step Response(s)")
+        return ax
 
-        return fig.axes
+    def step_response(self, stressmodels=None, **kwargs):
+        """Plot the block response for a specific stressmodels.
+
+        Parameters
+        ----------
+        stressmodels: list, optional
+            List with the stressmodels to plot the block response for.
+
+        Returns
+        -------
+        matplotlib.axes
+            matplotlib axes instance.
+
+        """
+        if not stressmodels:
+            stressmodels = self.ml.stressmodels.keys()
+
+        legend = []
+
+        ax = plt.subplot(**kwargs)
+
+        for name in stressmodels:
+            if hasattr(self.ml.stressmodels[name], 'rfunc'):
+                self.ml.get_step_response(name).plot(ax=ax)
+                legend.append(name)
+            else:
+                logger.warning("Stressmodel {} not in stressmodels "
+                               "list.".format(name))
+
+        plt.xlim(0)
+        plt.xlabel("Time [days]")
+        plt.legend(legend)
+        return ax
 
     @model_tmin_tmax
-    def stresses(self, tmin=None, tmax=None, cols=1, split=True,
-                 sharex=True, figsize=(10, 8), **kwargs):
+    def stresses(self, tmin=None, tmax=None, cols=1, split=True, sharex=True,
+                 figsize=(10, 8), **kwargs):
         """This method creates a graph with all the stresses used in the
          model.
 
@@ -447,7 +455,3 @@ class Plotting:
         fig.tight_layout(pad=0.0)
 
         return axes
-
-    def _get_figure(self, **kwargs):
-        fig = plt.figure(**kwargs)
-        return fig
