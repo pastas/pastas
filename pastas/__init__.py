6924f6f0fcb704788671212b6008556364d371bb
Raoul Collenteur <raoulcollenteur@gmail.com>
2018-09-28 17:26:23 +0200

    Rename rfunc Bruggeman to Polder

--- pastas/__init__.py
+++ pastas/__init__.py
@@ -4,7 +4,7 @@ from .model import Model
 from .noisemodels import NoiseModel, NoiseModel2
 from .project import Project
 from .read import read_meny, read_dino, read_knmi, read_waterbase
-from .rfunc import Gamma, Exponential, Hantush, Theis, Bruggeman, One
+from .rfunc import Gamma, Exponential, Hantush, Theis, Polder, One
 from .solver import LmfitSolve, LeastSquares, DESolve, MarkSolver
 from .stressmodels import StressModel, StressModel2, Constant
 from .timeseries import TimeSeries
