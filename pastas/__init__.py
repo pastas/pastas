2d7b16831be063d86f524d6761a31f93f2dd98a3
Raoul Collenteur <raoulcollenteur@gmail.com>
2019-02-06 10:41:25 +0100

    Remove Theis from __init__.py

--- pastas/__init__.py
+++ pastas/__init__.py
@@ -4,7 +4,7 @@ from .model import Model
 from .noisemodels import NoiseModel, NoiseModel2
 from .project import Project
 from .read import read_meny, read_dino, read_knmi, read_waterbase
-from .rfunc import Gamma, Exponential, Hantush, Theis, Polder, One
+from .rfunc import Gamma, Exponential, Hantush, Polder, One
 from .solver import LmfitSolve, LeastSquares, DESolve, MarkSolver
 from .stressmodels import StressModel, StressModel2, Constant, FactorModel
 from .timeseries import TimeSeries
