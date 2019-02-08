pastas/pastas

commit 58d959132959a8637c189862b6e0e35a3a5b12a6

Make sure the right odelt is used
By calculating it when needed.
This used to go wrong, when a noisemodel was used on a model with a daily frequency where the oseries consist of a more frequent (hourly) frequency.

pastas/model.py
@@ -75,7 +75,6 @@ class Model:
         # Construct the different model components
         self.oseries = TimeSeries(oseries, settings="oseries",
                                   metadata=metadata)
-        self.odelt = self.get_odelt()
 
         if name is None:
             name = self.oseries.name
@@ -469,7 +468,7 @@ class Model:
         res = self.residuals(parameters, tmin, tmax, freq, warmup)
 
         # Calculate the noise
-        noise = self.noisemodel.simulate(res, self.odelt.loc[res.index],
+        noise = self.noisemodel.simulate(res, self.get_odelt(),
                                          parameters[-self.noisemodel.nparam:])
         return noise
 
@@ -577,9 +576,6 @@ class Model:
         self.sim_index = None
         self.oseries_calib = None
         self.interpolate_simulation = None
-
-        # Calculate odelt
-        self.odelt = self.get_odelt()
 
         # Initialize parameters
         self.parameters = self.get_init_parameters(noise, initial)
@@ -876,7 +872,7 @@ class Model:
             Pandas Series object
 
         """
-        odelt = self.oseries.series.index.to_series().diff() / \
+        odelt = self.observations().index.to_series().diff() / \
                 pd.Timedelta(1, freq)
         return odelt
 

examples/example_900.py
@@ -1,0 +1,30 @@
+"""
+This test file is meant for developing purposes. Providing an easy method to
+test the functioning of PASTA during development.
+
+"""
+import pastas as ps
+import pandas as pd
+
+# read observations
+obs = ps.read_dino('data/B58C0698001_1.csv')
+
+# Create the time series model
+ml = ps.Model(obs)
+
+# read weather data
+rain = ps.read.KnmiStation.fromfile('data/neerslaggeg_HEIBLOEM-L_967-2.txt')
+rain = ps.TimeSeries(rain.data.RD,settings='prec')
+
+evap = ps.read_knmi('data/etmgeg_380.txt', variables='EV24')
+evap = pd.Series(evap.values,evap.index+pd.to_timedelta(9,unit='h'))
+evap = ps.TimeSeries(evap,settings='evap')
+
+# Create stress
+sm = ps.StressModel2(stress=[rain, evap], rfunc=ps.Exponential,
+                     name='recharge')
+ml.add_stressmodel(sm)
+
+## Solve
+ml.solve()
+ml.plot()

commit 7e1c319e255ad157951986e856a3608cbfd90eb7

Simplify the noise calculation
Commit #58d9591 introduced problems when tmin was changed found in example notebook 5. However, the calculation was overly complicated and prone to errors already.

odelt can more safely and easily extracted from the index of the residual series.

pastas/model.py
@@ -109,7 +109,6 @@ class Model:
                                 name="constant")
             self.add_constant(constant)
         if noisemodel:
-            noisemodel
             self.add_noisemodel(NoiseModel())
 
         # File Information
@@ -470,6 +469,9 @@ class Model:
                               "no noisemodel.")
             return None
 
+        if freq is None:
+            freq = self.settings["freq"]
+
         # Get parameters if none are provided
         if parameters is None:
             parameters = self.get_parameters()
@@ -478,8 +480,9 @@ class Model:
         res = self.residuals(parameters, tmin, tmax, freq, warmup)
 
         # Calculate the noise
-        noise = self.noisemodel.simulate(res, self.get_odelt(),
-                                         parameters[-self.noisemodel.nparam:])
+        noise = self.noisemodel.simulate(res,
+                                         parameters[-self.noisemodel.nparam:],
+                                         freq=freq)
         return noise
 
     def observations(self, tmin=None, tmax=None, freq=None):
@@ -867,24 +870,6 @@ class Model:
         else:
             sim_index = self.sim_index
         return sim_index
-
-    def get_odelt(self, freq="D"):
-        """Internal method to get the timesteps between the observations.
-
-        Parameters
-        ----------
-        freq: str
-            Frequency string.
-
-        Returns
-        -------
-        odelt: pandas.Series
-            Pandas Series object
-
-        """
-        odelt = self.observations().index.to_series().diff() / \
-                pd.Timedelta(1, freq)
-        return odelt
 
     def get_tmin(self, tmin=None, freq=None, use_oseries=True,
                  use_stresses=False):

pastas/noisemodels.py
@@ -125,7 +125,7 @@ class NoiseModel(NoiseModelBase):
     def set_init_parameters(self):
         self.parameters.loc['noise_alpha'] = (14.0, 0, 5000, 1, 'noise')
 
-    def simulate(self, res, odelt, parameters):
+    def simulate(self, res, parameters, freq):
         """
 
         Parameters
@@ -143,6 +143,7 @@ class NoiseModel(NoiseModelBase):
             Series of the noise.
 
         """
+        odelt = res.index.to_series().diff() / pd.Timedelta(1, freq)
         odelt = odelt.iloc[1:]
         alpha = parameters[0]
         noise = pd.Series(data=res)
@@ -210,7 +211,7 @@ class NoiseModel2(NoiseModelBase):
     def set_init_parameters(self):
         self.parameters.loc['noise_alpha'] = (14.0, 0, 5000, 1, 'noise')
 
-    def simulate(self, res, odelt, parameters):
+    def simulate(self, res, parameters, freq):
         """
 
         Parameters
@@ -228,9 +229,11 @@ class NoiseModel2(NoiseModelBase):
             Series of the noise.
 
         """
+        odelt = res.index.to_series().diff() / pd.Timedelta(1, freq)
+        odelt = odelt.iloc[1:]
         noise = pd.Series(res)
         alpha = parameters[0]
         # res.values is needed else it gets messed up with the dates
-        noise.iloc[1:] -= np.exp(-odelt.iloc[1:] / alpha) * res.values[:-1]
+        noise.iloc[1:] -= np.exp(-odelt/ alpha) * res.values[:-1]
         noise.name = "Noise"
         return noise