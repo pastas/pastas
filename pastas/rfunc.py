42441751a681096b4702292032773d03592a1520
saklop <48240574+saklop@users.noreply.github.com>
2019-04-10 17:51:31 +0200

    Update rfunc.py

--- pastas/rfunc.py
+++ pastas/rfunc.py
@@ -407,7 +407,8 @@ class FourParam(RfuncBase):
     -----
 
     .. math::
-        step(t) = A / quad(t**n * np.exp(-t/a - b/t),0,np.inf) * quad(t**n * np.exp(-t/a - b/t),0,t)
+        step(t) = A / quad(t**n * np.exp(-t/a - b/t),0,np.inf) *
+                      quad(t**n * np.exp(-t/a - b/t),0,t)
 
     """
     _name = "FourParam"
@@ -415,6 +416,7 @@ class FourParam(RfuncBase):
     def __init__(self, up=True, meanstress=1, cutoff=0.99):
         RfuncBase.__init__(self, up, meanstress, cutoff)
         self.nparam = 4
+        self.quad = False
 
     def set_parameters(self, name):
         parameters = DataFrame(
@@ -425,106 +427,148 @@ class FourParam(RfuncBase):
         else:
             parameters.loc[name + '_A'] = (-1 / self.meanstress,
                                            -100 / self.meanstress, 0, 1, name)
-        # if n is too small, the length of the response function is close to zero
         parameters.loc[name + '_n'] = (1, -10, 10, 1, name)
         parameters.loc[name + '_a'] = (10, 0.01, 5000, 1, name)
         parameters.loc[name + '_b'] = (10, 0.01, 5000, 1, name)
         return parameters
 
     def function(self, t, p):
-        return (t ** p[1]) * np.exp(-t / p[2] - p[3] / t)
+        return (t ** (p[1] - 1)) * np.exp(-t / p[2] - p[3] / t)
 
     def get_tmax(self, p, cutoff=None):
         if cutoff is None:
             cutoff = self.cutoff
 
-        t1 = -np.sqrt(3 / 5)
-        t2 = 0
-        t3 = np.sqrt(3 / 5)
-        w1 = 5 / 9
-        w2 = 8 / 9
-        w3 = 5 / 9
+        if self.quad:
+            x = np.arange(1, 10000, 1)
+            y = np.zeros_like(x)
+            func = self.function(x, p)
+            func_half = self.function(x[:-1] + 1 / 2, p)
+            y[1:] = y[0] + np.cumsum(1 / 6 *
+                                     (func[:-1] + 4 * func_half + func[1:]))
+            y = y / quad(self.function, 0, np.inf, args=p)[0]
+            return np.searchsorted(y, cutoff)
 
-        x = np.arange(1, 10000, 1)
-        y = np.zeros_like(x)
-        func = self.function(x, p)
-        func_half = self.function(x[:-1] + 1 / 2, p)
-        y[0] = 0.5 * (w1 * self.function(0.5 * t1 + 0.5, p) + w2 * self.function(0.5 * t2 + 0.5, p) + w3 * self.function(0.5 * t3 + 0.5, p))
-        y[1:] = y[0] + np.cumsum(1 / 6 * (func[:-1] + 4 * func_half + func[1:]))
-        y = y / quad(self.function, 0, np.inf, args=p)[0]
-        return np.searchsorted(y, cutoff)
+        else:
+            t1 = -np.sqrt(3 / 5)
+            t2 = 0
+            t3 = np.sqrt(3 / 5)
+            w1 = 5 / 9
+            w2 = 8 / 9
+            w3 = 5 / 9
 
-    def get_tmax_quad(self, p, cutoff=None):
-        if cutoff is None:
-            cutoff = self.cutoff
-        x = np.arange(1, 10000, 1)
-        y = np.zeros_like(x)
-        func = self.function(x, p)
-        func_half = self.function(x[:-1] + 1 / 2, p)
-        y[1:] = y[0] + np.cumsum(1 / 6 * (func[:-1] + 4 * func_half + func[1:]))
-        y = y / quad(self.function, 0, np.inf, args=p)[0]
-        return (np.searchsorted(y, cutoff))
+            x = np.arange(1, 10000, 1)
+            y = np.zeros_like(x)
+            func = self.function(x, p)
+            func_half = self.function(x[:-1] + 1 / 2, p)
+            y[0] = 0.5 * (w1 * self.function(0.5 * t1 + 0.5, p) +
+                          w2 * self.function(0.5 * t2 + 0.5, p) +
+                          w3 * self.function(0.5 * t3 + 0.5, p))
+            y[1:] = y[0] + np.cumsum(1 / 6 *
+                                     (func[:-1] + 4 * func_half + func[1:]))
+            y = y / quad(self.function, 0, np.inf, args=p)[0]
+            return np.searchsorted(y, cutoff)
 
     def gain(self, p):
         return p[0]
 
     def step(self, p, dt=1, cutoff=0.99):
 
-        t1 = -np.sqrt(3 / 5)
-        t2 = 0
-        t3 = np.sqrt(3 / 5)
-        w1 = 5 / 9
-        w2 = 8 / 9
-        w3 = 5 / 9
-
-        if dt > 0.1: # if step size > 0.1 the step size is reduced in the integration to improve integration result
-            step = 0.1 # step size for numerical integration
-            self.tmax = max(self.get_tmax(p, cutoff), 3 * step)
-            t = np.arange(step, self.tmax, step)
-            s = np.zeros_like(t)
-
-            # for interval [0,dt] https://en.wikipedia.org/wiki/Gaussian_quadrature:
-            s[0] = (step/2) * (w1 * self.function((step/2) * t1 + (step/2), p) + w2 * self.function((step/2) * t2 + (step/2), p) + w3 * self.function((step/2) * t3 + (step/2), p))
-
-            # for interval [dt,tmax]:
-            func = self.function(t, p)
-            func_half = self.function(t[:-1] + step/2, p)
-            s[1:] = s[0] + np.cumsum(step / 6 * (func[:-1] + 4 * func_half + func[1:]))
-            s = s * (p[0] / quad(self.function, 0, np.inf, args=p)[0])
-            return s[int(dt/step-1)::int(dt/step)]
-        else:
+        if self.quad:
             if isinstance(dt, np.ndarray):
                 t = dt
             else:
                 self.tmax = max(self.get_tmax(p, cutoff), 3 * dt)
                 t = np.arange(dt, self.tmax, dt)
             s = np.zeros_like(t)
-
-            # for interval [0,dt] Gaussian quadrate:
-            s[0] = (dt/2) * (w1 * self.function((dt/2) * t1 + (dt/2), p) + w2 * self.function((dt/2) * t2 + (dt/2), p) + w3 * self.function((dt/2) * t3 + (dt/2), p))
-
-            # for interval [dt,tmax] Simpson integration:
-            func = self.function(t, p)
-            func_half = self.function(t[:-1] + dt / 2, p)
-            s[1:] = s[0] + np.cumsum(dt / 6 * (func[:-1] + 4 * func_half + func[1:]))
-            s = s * (p[0] / quad(self.function, 0, np.inf, args=p)[0])
+            s[0] = quad(self.function, 0, dt, args=p)[0]
+            for i in range(1, len(t)):
+                s[i] = s[i - 1] + quad(self.function, t[i - 1], t[i], args=p)[0]
+            s = s * (p[0] / (quad(self.function, 0, np.inf, args=p))[0])
             return s
 
-    def step_quad(self, p, dt=1, cutoff=0.99):
-        if isinstance(dt, np.ndarray):
-            t = dt
         else:
-            self.tmax = max(self.get_tmax_quad(p, cutoff), 3 * dt)
-            t = np.arange(dt, self.tmax, dt)
-        #         def function(t):
-        #             return (t**p[1]) * np.exp(-t/p[2] - p[3]/t)
-        s = np.zeros_like(t)
-        s[0] = quad(self.function, 0, dt, args=p)[0]
-        for i in range(1, len(t)):
-            s[i] = s[i - 1] + quad(self.function, t[i - 1], t[i], args=p)[0]
-        s = s * (p[0] / (quad(self.function, 0, np.inf, args=p))[0])
-        return s
 
+            t1 = -np.sqrt(3 / 5)
+            t2 = 0
+            t3 = np.sqrt(3 / 5)
+            w1 = 5 / 9
+            w2 = 8 / 9
+            w3 = 5 / 9
+
+            if dt > 0.1:
+                step = 0.1  # step size for numerical integration
+                self.tmax = max(self.get_tmax(p, cutoff), 3 * step)
+                t = np.arange(step, self.tmax, step)
+                s = np.zeros_like(t)
+
+                # for interval [0,dt] :
+                s[0] = (step / 2) * \
+                       (w1 * self.function((step / 2) * t1 + (step / 2), p) +
+                        w2 * self.function((step / 2) * t2 + (step / 2), p) +
+                        w3 * self.function((step / 2) * t3 + (step / 2), p))
+
+                # for interval [dt,tmax]:
+                func = self.function(t, p)
+                func_half = self.function(t[:-1] + step / 2, p)
+                s[1:] = s[0] + np.cumsum(step / 6 *
+                                         (func[:-1] + 4 * func_half + func[1:]))
+                s = s * (p[0] / quad(self.function, 0, np.inf, args=p)[0])
+                return s[int(dt / step - 1)::int(dt / step)]
+            else:
+                if isinstance(dt, np.ndarray):
+                    t = dt
+                else:
+                    self.tmax = max(self.get_tmax(p, cutoff), 3 * dt)
+                    t = np.arange(dt, self.tmax, dt)
+                s = np.zeros_like(t)
+
+                # for interval [0,dt] Gaussian quadrate:
+                s[0] = (dt / 2) * \
+                       (w1 * self.function((dt / 2) * t1 + (dt / 2), p) +
+                        w2 * self.function((dt / 2) * t2 + (dt / 2), p) +
+                        w3 * self.function((dt / 2) * t3 + (dt / 2), p))
+
+                # for interval [dt,tmax] Simpson integration:
+                func = self.function(t, p)
+                func_half = self.function(t[:-1] + dt / 2, p)
+                s[1:] = s[0] + np.cumsum(dt / 6 *
+                                         (func[:-1] + 4 * func_half + func[1:]))
+                s = s * (p[0] / quad(self.function, 0, np.inf, args=p)[0])
+                return s
+
+
+class FourParamQuad(FourParam):
+    """"Four Parameter response function with 4 parameters A, a, b, and n.
+
+    Parameters
+    ----------
+    up: bool, optional
+        indicates whether a positive stress will cause the head to go up
+        (True, default) or down (False)
+    meanstress: float
+        mean value of the stress, used to set the initial value such that
+        the final step times the mean stress equals 1
+    cutoff: float
+        percentage after which the step function is cut off. default=0.99.
+
+    Notes
+    -----
+    This response function uses np.quad to integrate the Four Parameter response
+    function, which requires more calculation time.
+    This response function can be used for testing purposes.
+
+    .. math::
+        step(t) = A / quad(t**n * np.exp(-t/a - b/t),0,np.inf) *
+                      quad(t**n * np.exp(-t/a - b/t),0,t)
+
+    """
+    _name = "FourParam"
+
+    def __init__(self, up=True, meanstress=1, cutoff=0.99):
+        FourParam.__init__(self, up, meanstress, cutoff)
+        self.nparam = 4
+        self.quad = True
 class DoubleExponential(RfuncBase):
     """Gamma response function with 3 parameters A, a, and n.
 
