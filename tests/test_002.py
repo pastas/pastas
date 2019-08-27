0e9ad186af880ea4aa3ca0344372aae4ea032e93
Raoul Collenteur <raoulcollenteur@gmail.com>
2019-08-27 18:40:16 +0200

    Update tests for travis

--- tests/test_002.py
+++ tests/test_002.py
@@ -1,24 +1,47 @@
 import pastas as ps
 
 
-def test_model():
-    # Import and check the observed groundwater time series
+def test_create_model():
     obs = ps.read_dino('tests/data/dino_gwl_data.csv')
-
-    # Create the time series model
     ml = ps.Model(obs, name="Test_Model")
+    return ml
 
-    # read weather data
-    rain = ps.read_knmi('tests/data/knmi_rain_data.txt',
-                        variables='RD')
+
+def test_add_stressmodel():
+    ml = test_create_model()
+    rain = ps.read_knmi('tests/data/knmi_rain_data.txt', variables='RD')
     evap = ps.read_knmi('tests/data/knmi_evap_data.txt', variables='EV24')
-
-    ## Create stress
     sm = ps.StressModel2(stress=[rain, evap], rfunc=ps.Exponential,
                          name='recharge')
     ml.add_stressmodel(sm)
+    return ml
 
-    # Solve the time series model
+
+def test_del_stressmodel():
+    ml = test_add_stressmodel()
+    ml.del_stressmodel("recharge")
+    return
+
+
+def test_solve_model():
+    ml = test_add_stressmodel()
     ml.solve()
+    return
 
-    return 'model succesfull'
+
+def test_save_model():
+    ml = test_create_model()
+    ml.to_file("test.pas")
+    return
+
+
+def test_load_model():
+    test_save_model()
+    ml = ps.io.load("test.pas")
+    return
+
+
+def test_model_copy():
+    ml = test_create_model()
+    ml.copy()
+    return
--- tests/test_recharge.py
+++ tests/test_recharge.py
@@ -1,22 +1,29 @@
 import pastas as ps
-import pandas as pd
 
 
-def test_linear():
-    index = pd.date_range("2000-01-01", "2000-01-10")
-    prec = pd.Series([1, 2] * 5, index=index)
-    evap = prec / 2
-    rm = ps.RechargeModel(prec=prec, evap=evap, rfunc=ps.Exponential,
-                          recharge="Linear", name="recharge")
+def test_create_rechargemodel():
+    rain = ps.read_knmi('tests/data/knmi_rain_data.txt', variables='RD')
+    evap = ps.read_knmi('tests/data/knmi_evap_data.txt', variables='EV24')
+    rm = ps.RechargeModel(prec=rain, evap=evap, name='recharge',
+                          recharge="Linear")
     return rm
 
 
-def test_get_stress():
-    rm = test_linear()
-    rm.get_stress()
+def test_create_model():
+    obs = ps.read_dino('tests/data/dino_gwl_data.csv')
+    ml = ps.Model(obs, name="Test_Model")
+    sm = test_create_rechargemodel()
+    ml.add_stressmodel(sm)
+    return ml
+
+
+def test_model_solve():
+    ml = test_create_model()
+    ml.solve()
     return
 
-def test_copy():
-    rm = test_linear()
-    rm.copy()
+
+def test_model_copy():
+    ml = test_create_model()
+    ml.copy()
     return
