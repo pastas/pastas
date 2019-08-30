9abce17371ac1b2a0e29a90b39ce8560f05bf47e
Raoul Collenteur <raoulcollenteur@gmail.com>
2019-08-30 10:14:14 +0200

    Prepare update Master to 0.12.0

--- pastas/version.py
+++ pastas/version.py
@@ -1,3 +1,3 @@
 # This is the only location where the version will be written and changed.
 # Based on https://packaging.python.org/single_source_version/
-__version__ = '0.12.0b'
+__version__ = '0.12.0'
