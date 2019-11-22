dcf37039f741902900fb6bbde1d41a9146a5d492
Raoul Collenteur <raoulcollenteur@gmail.com>
2019-11-22 09:22:40 +0100

    fix example_well

--- examples/example_well.py
+++ examples/example_well.py
@@ -24,12 +24,11 @@ evap = pd.read_csv("notebooks/data_notebook_7/evap_wellex.csv",
 
 # Create stress
 rm = ps.RechargeModel(prec=rain, evap=evap, rfunc=ps.Exponential,
-                      recharge=ps.rch.FlexModel(preferential=False),
                       name='recharge')
 ml.add_stressmodel(rm)
 
 well = pd.read_csv("notebooks/data_notebook_7/well_wellex.csv",
-                   index_col="Date", parse_dates=True)/1e6
+                   index_col="Date", parse_dates=True)
 sm = ps.StressModel(well, rfunc=ps.Exponential, name="well", up=False)
 ml.add_stressmodel(sm)
 
@@ -37,4 +36,3 @@ ml.add_stressmodel(sm)
 ml.solve(noise=True)
 ml.plots.results()
 
-df = ml.fit.prediction_interval()
