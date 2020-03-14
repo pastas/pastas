af5a5cd5836d191f3ba39f7c498536883a6ccc96
Raoul Collenteur <raoulcollenteur@gmail.com>
2020-03-14 19:47:43 +0100

    Update test_recharge.py
    #173

--- tests/test_recharge.py
+++ tests/test_recharge.py
@@ -2,32 +2,58 @@ from pandas import read_csv
 
 import pastas as ps
 
+# Load series before
+rain = read_csv("tests/data/rain.csv", index_col=0, parse_dates=True,
+                squeeze=True)
+evap = read_csv("tests/data/evap.csv", index_col=0, parse_dates=True,
+                squeeze=True)
+obs = read_csv("tests/data/obs.csv", index_col=0, parse_dates=True,
+               squeeze=True)
+
 
 def test_create_rechargemodel():
-    rain = read_csv("tests/data/rain.csv", index_col=0, parse_dates=True,
-                    squeeze=True)
-    evap = read_csv("tests/data/evap.csv", index_col=0, parse_dates=True,
-                    squeeze=True)
-    rm = ps.RechargeModel(prec=rain, evap=evap, name='recharge')
+    rm = ps.RechargeModel(prec=rain, evap=evap)
     return rm
 
 
-def test_create_model():
-    obs = read_csv("tests/data/obs.csv", index_col=0, parse_dates=True,
-                   squeeze=True)
-    ml = ps.Model(obs, name="Test_Model")
-    sm = test_create_rechargemodel()
-    ml.add_stressmodel(sm)
+def test_default_model():
+    ml = ps.Model(obs, name="rch_model")
+    rm = test_create_rechargemodel()
+    ml.add_stressmodel(rm)
     return ml
 
 
 def test_model_solve():
-    ml = test_create_model()
+    ml = test_default_model()
     ml.solve()
     return
 
 
 def test_model_copy():
-    ml = test_create_model()
+    ml = test_default_model()
     ml.copy()
+    return
+
+
+def test_flexmodel():
+    ml = ps.Model(obs, name="rch_model")
+    rm = ps.RechargeModel(prec=rain, evap=evap, recharge=ps.rch.FlexModel)
+    ml.add_stressmodel(rm)
+    ml.solve()
+    return
+
+
+def test_berendrecht():
+    ml = ps.Model(obs, name="rch_model")
+    rm = ps.RechargeModel(prec=rain, evap=evap, recharge=ps.rch.Berendrecht)
+    ml.add_stressmodel(rm)
+    ml.solve()
+    return
+
+
+def test_linear():
+    ml = ps.Model(obs, name="rch_model")
+    rm = ps.RechargeModel(prec=rain, evap=evap, recharge=ps.rch.Linear)
+    ml.add_stressmodel(rm)
+    ml.solve()
     return
