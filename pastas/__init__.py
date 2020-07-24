045af7a2f21a78c653126858b01aba6ef4182de0
Steijn van <steit1@bw.local>
2020-07-24 17:03:42 +0200

    top levels imports for reading river level gauges in Dinoloket format

--- pastas/__init__.py
+++ pastas/__init__.py
@@ -5,7 +5,7 @@ from .model import Model
 from .noisemodels import NoiseModel, NoiseModel2, ArmaModel, NoiseModel3
 from .plots import TrackSolve
 from .project import Project
-from .read import read_meny, read_dino, read_knmi, read_waterbase
+from .read import read_meny, read_dino, read_dino_level_gauge, read_knmi, read_waterbase
 from .rfunc import Gamma, Exponential, Hantush, Polder, One, FourParam, \
     DoubleExponential, HantushWellModel, Hantush2, Polder2
 from .solver import LmfitSolve, LeastSquares
