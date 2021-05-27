a62fd94f716092741d7393044b83d2ea1a79495b
Raoul Collenteur <raoulcollenteur@gmail.com>
2021-05-27 15:10:25 +0200

    Update snow.py

--- examples/snow.py
+++ examples/snow.py
@@ -2,6 +2,7 @@ import pandas as pd
 import pastas as ps
 import matplotlib.pyplot as plt
 
+ps.set_log_level("WARNING")
 
 # head = pd.read_csv("notebooks/data_wagna/head_wagna.csv", index_col=0,
 #                    parse_dates=True, squeeze=True, skiprows=2).loc["2006":]
@@ -22,21 +23,19 @@ temp = pd.read_csv("data/heby/TG_STAID000426.txt", index_col=0,
                    usecols=["DATE", "TG"], skipinitialspace=True,
                    parse_dates=True, squeeze=True, skiprows=19) / 10
 
-
-
-
 #temp = temp.asfreq("D").fillna(0)
 tmin = pd.Timestamp("1990-01-01")  # Needs warmup
 tmax = pd.Timestamp("2010-12-31")
 
 ml = ps.Model(head)
-sm = ps.RechargeModel(rain, evap, recharge=ps.rch.FlexSnowModel(),
+sm = ps.RechargeModel(rain, evap, recharge=ps.rch.FlexSnowModel(snow=True),
                       rfunc=ps.Exponential, name="rch", temp=temp)
 ml.add_stressmodel(sm)
 
 # In case of the non-linear model, change some parameter settings
 ml.set_parameter("rch_srmax", vary=False)
 ml.set_parameter("rch_k", vary=True)
+
 #ml.set_parameter("constant_d", vary=True, initial=262, pmax=head.min())
 
 # Solve the Pastas model
