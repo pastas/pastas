fix #317 improve atol calculation for bounds check for case pmin=np.nan

pastas/model.py
@@ -1657,7 +1657,7 @@ class Model:
             # calculate atol based on minimum, with max 1e-8
             # otherwise set 1 order of magnitude lower than minimum value
             min_val = np.abs(pmin)
-            if min_val == 0.0:
+            if min_val == 0.0 or np.isnan(pmin):
                 atol = 1e-8
             else:
                 atol = np.min([1e-8, 10**(np.floor(np.log10(min_val)) - 1)])