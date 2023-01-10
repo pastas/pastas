4e2b8b0bd062fad7fc47edc7abfe0b173566ba9f
Raoul Collenteur <raoulcollenteur@gmail.com>
2023-01-10 11:01:34 +0100

    bump minimum scipy to 1.8 to fix #177
    Bumped minimum Scipy function to 1.8 to fix issue #177. Closes #177

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     install_requires=['numpy>=1.17, <1.24',
                       'matplotlib>=3.1',
                       'pandas>=1.1',
-                      'scipy>=1.3'],
+                      'scipy>=1.8'],
     packages=find_packages(exclude=[]),
     extras_require={
         "full": ["numba>=0.51", "lmfit>=1.0.0"],
