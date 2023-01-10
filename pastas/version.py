54759e0eadf81200dd9afb895bf7f16c3f05289a
Martin Vonk <vonk.mart@gmail.com>
2023-01-10 13:23:06 +0100

    get version using importlib
    since python 3.7 is deprecated in pastas 0.23.0

--- pastas/version.py
+++ pastas/version.py
@@ -1,19 +1,3 @@
-from platform import python_version
+from importlib.metadata import version
 
-
-def get_pastas_version() -> str:
-    if python_version() < "3.8.0":
-        __version__ = None
-        with open("../../pyproject.toml") as fo:
-            l = fo.readline()
-            while l:
-                if "version" in l:
-                    __version__ = l.split()[-1].strip().strip('"')
-                    break
-                l = fo.readline()
-        if __version__ is None:
-            raise ValueError("No version found in pyproject.toml")
-    else:
-        from importlib.metadata import version
-        __version__ = version("pastas")
-    return __version__
+__version__ = version("pastas")
