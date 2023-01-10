7acc5046afaf9c32625d8d75ff2acf0e39ec7f87
Raoul Collenteur <raoulcollenteur@gmail.com>
2023-01-10 10:49:25 +0100

    wait with Python 3.11 support
    As noted by @martinvonk , Numba is not ready yet, let's try again before the next release.

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
         "Topic :: Scientific/Engineering :: Hydrology",
     ],
     platforms='Windows, Mac OS-X',
