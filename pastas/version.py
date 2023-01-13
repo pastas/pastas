aeb557e067858ad0062816b6426d1d2d4e52069b
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2023-01-13 15:12:58 +0100

    black version.py

--- pastas/version.py
+++ pastas/version.py
@@ -10,8 +10,10 @@ def check_numba() -> None:
     try:
         import_module("numba")
     except ModuleNotFoundError:
-        logger.warning("Numba is not installed. Installing Numba is "
-                       "recommended for significant speed-ups.")
+        logger.warning(
+            "Numba is not installed. Installing Numba is "
+            "recommended for significant speed-ups."
+        )
 
 
 def check_numba_scipy() -> bool:
@@ -24,9 +26,7 @@ def check_numba_scipy() -> bool:
         return False
 
     scipy_version = metadata.version("scipy")
-    scipy_version_nsc = [
-        x for x in metadata.requires("numba-scipy") if "scipy" in x
-    ]
+    scipy_version_nsc = [x for x in metadata.requires("numba-scipy") if "scipy" in x]
     scipy_version_nsc = scipy_version_nsc[0].split(",")[0].split("=")[-1]
     if scipy_version > scipy_version_nsc:
         logger.warning(
@@ -47,11 +47,13 @@ def show_versions(lmfit: bool = True, numba: bool = True) -> None:
         Print the version of Numba. Needs to be installed.
     """
 
-    msg = (f"Python version: {python_version()}\n"
-           f"NumPy version: {metadata.version('numpy')}\n"
-           f"Pandas version: {metadata.version('pandas')}\n"
-           f"SciPy version: {metadata.version('scipy')}\n"
-           f"Matplotlib version: {metadata.version('matplotlib')}")
+    msg = (
+        f"Python version: {python_version()}\n"
+        f"NumPy version: {metadata.version('numpy')}\n"
+        f"Pandas version: {metadata.version('pandas')}\n"
+        f"SciPy version: {metadata.version('scipy')}\n"
+        f"Matplotlib version: {metadata.version('matplotlib')}"
+    )
 
     if lmfit:
         msg += "\nLMfit version: "
