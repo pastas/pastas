a1e5f1924ac8c4d393aecd9cfb5b26872af55411
dbrakenhoff <d.brakenhoff@artesia-water.nl>
2023-01-16 17:30:52 +0100

    update notebooks/examples - remove timestamp_weighted_resample_fast from example (for now) - update 00_prepare_timeseries notebook - small spelling changes to the rest.

--- doc/examples/example_timestep_weighted_resample.py
+++ doc/examples/example_timestep_weighted_resample.py
@@ -2,9 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-from pastas.ts_processing import (
+from pastas.timeseries_utils import (
     timestep_weighted_resample,
-    timestep_weighted_resample_fast,
 )
 
 # make a daily series from monthly (mostly) values, without specifying the
@@ -14,12 +13,12 @@ index = pd.date_range("2000-1-1", "2001-1-1", freq="MS")
 series0 = pd.Series(np.random.rand(len(index)), index)
 series = series0.resample("d").mean()
 series = timestep_weighted_resample(series0, series.index)
-series2 = timestep_weighted_resample_fast(series0, "d")
+# series2 = timestep_weighted_resample_fast(series0, "d")
 
 plt.figure()
 series0.plot(label="Monthly (mostly)")
 series.plot(label="Daily")
-series2.plot(label="Daily (fast)", linestyle="--")
+# series2.plot(label="Daily (fast)", linestyle="--")
 plt.legend()
 
 # make a precipitation-series at 0:00 from values at 9:00
@@ -27,9 +26,9 @@ plt.legend()
 index = pd.date_range("2000-1-1 9:00", "2000-1-10 9:00")
 series0 = pd.Series(np.random.rand(len(index)), index)
 series = timestep_weighted_resample(series0, series0.index.normalize())
-series2 = timestep_weighted_resample_fast(series0, "d")
+# series2 = timestep_weighted_resample_fast(series0, "d")
 plt.figure()
 series0.plot(label="Original (9:00)")
 series.plot(label="Resampled (0:00)")
-series2.plot(label="Resampled (0:00, fast)", linestyle="--")
+# series2.plot(label="Resampled (0:00, fast)", linestyle="--")
 plt.legend()
