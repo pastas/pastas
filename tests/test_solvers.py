75fbb603d2c1088252911ee169fdb7cb7d0f10e0
Raoul Collenteur <raoulcollenteur@gmail.com>
2023-01-19 16:17:24 +0100

    Fix GH actions errors for tests

--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -38,25 +38,30 @@ def test_no_noise():
 
 # test the uncertainty method here
 def test_pred_interval():
-    ml = test_least_squares()
+    ml = create_model()
+    ml.solve(solver=ps.LeastSquares)
     ml.fit.prediction_interval(n=10)
 
 
 def test_ci_simulation():
-    ml = test_least_squares()
+    ml = create_model()
+    ml.solve(solver=ps.LeastSquares)
     ml.fit.ci_simulation(n=10)
 
 
 def test_ci_block_response():
-    ml = test_least_squares()
+    ml = create_model()
+    ml.solve(solver=ps.LeastSquares)
     ml.fit.ci_block_response(name="recharge", n=10)
 
 
 def test_ci_step_response():
-    ml = test_least_squares()
+    ml = create_model()
+    ml.solve(solver=ps.LeastSquares)
     ml.fit.ci_step_response(name="recharge", n=10)
 
 
 def test_ci_contribution():
-    ml = test_least_squares()
+    ml = create_model()
+    ml.solve(solver=ps.LeastSquares)
     ml.fit.ci_contribution(name="recharge", n=10)
