19d2fd1d9edd44d2dea6201bc29ec390602f80c4
Martin Vonk <66305055+martinvonk@users.noreply.github.com>
2023-02-15 11:20:49 +0100

    Update types.py

--- pastas/typing/types.py
+++ pastas/typing/types.py
@@ -5,8 +5,8 @@ from typing import TYPE_CHECKING, Any, Callable, TypeVar, Union
 
 # External libraries
 # Matplotlib
-from matplotlib.axes._base import _AxesBase
-from matplotlib.figure import FigureBase
+from matplotlib.axes import Axes as MatplotlibAxes
+from matplotlib.figure import Figure as MatplotlibFigure
 
 # Numpy
 from numpy.typing import ArrayLike as NumpyArrayLike
@@ -15,8 +15,8 @@ from numpy.typing import ArrayLike as NumpyArrayLike
 from pandas import Timestamp
 
 # External Types
-Axes = TypeVar("Axes", bound=_AxesBase)  # Matplotlib Axes
-Figure = TypeVar("Figure", bound=FigureBase)  # Matplotlib Figure
+Axes = TypeVar("Axes", bound=MatplotlibAxes)  # Matplotlib Axes
+Figure = TypeVar("Figure", bound=MatplotlibFigure)  # Matplotlib Figure
 ArrayLike = TypeVar("ArrayLike", bound=NumpyArrayLike)  # Array Like (NumPy based)
 
 # Internal library
