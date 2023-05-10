952b18f9855469ba9bf6a721d18a56a4564c22ef
Martin Vonk <vonk.mart@gmail.com>
2023-05-10 11:20:06 +0200

    test emcee_uncertainty notebook
    only tested once for github actions (for coverage), also is much faster due to 20 iterations

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -19,29 +19,26 @@ def test_notebook(file) -> None:
     cwd = os.getcwd()
 
     os.chdir(pathname)
-    if file not in [
-        "emcee_uncertainty.ipynb",
-    ]:
-        try:
-            # run autotest on each notebook
-            cmd = (
-                "jupyter "
-                + "nbconvert "
-                + "--ExecutePreprocessor.timeout=600 "
-                + "--to "
-                + "notebook "
-                + "--execute "
-                + '"{}" '.format(file)
-                + "--output-dir "
-                + "{} ".format(testdir)
-            )
-            ival = os.system(cmd)
-            msg = "could not run {}".format(file)
-            assert ival == 0, msg
-            assert os.path.isfile(os.path.join(testdir, file)), msg
-        except Exception as e:
-            os.chdir(cwd)
-            raise Exception(e)
+    try:
+        # run autotest on each notebook
+        cmd = (
+            "jupyter "
+            + "nbconvert "
+            + "--ExecutePreprocessor.timeout=600 "
+            + "--to "
+            + "notebook "
+            + "--execute "
+            + '"{}" '.format(file)
+            + "--output-dir "
+            + "{} ".format(testdir)
+        )
+        ival = os.system(cmd)
+        msg = "could not run {}".format(file)
+        assert ival == 0, msg
+        assert os.path.isfile(os.path.join(testdir, file)), msg
+    except Exception as e:
+        os.chdir(cwd)
+        raise Exception(e)
     os.chdir(cwd)
 
 
