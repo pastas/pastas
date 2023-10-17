3404be1a7defc2a46c306896ba36f7fce37e74e7
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2023-10-17 19:01:29 +0200

    remove commented code

--- tests/fake_plugin.py
+++ tests/fake_plugin.py
@@ -5,13 +5,3 @@ class FakeStressModel:
     def __init__(self):
         """I am a Fake StressModel, do not use me!"""
         pass
-
-
-# class StressModel:
-#     """I am a badly named plugin class as I conflict with a pastas default."""
-
-#     def __init__(self):
-#         """I am a badly named StressModel, do not use me!"""
-#         pass
-
-# %%
