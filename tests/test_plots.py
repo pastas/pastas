3e31c751475ffa553d7a24f6820d35edfa97ea63
Martin Vonk <vonk.mart@gmail.com>
2023-11-28 20:42:48 +0100

    Update test_plots.py

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -1,7 +1,7 @@
 from pandas import Series
 
 from pastas import Model
-from pastas.plots import TrackSolve, compare, pairplot
+from pastas.plotting.plots import TrackSolve, compare, pairplot
 
 # mpl.use("Agg")  # prevent _tkinter.TclError: Can't find a usable tk.tcl error
 
