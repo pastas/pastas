56af30368132652a52c8f267e66450de56aea0c4
Raoul Collenteur <raoulcollenteur@gmail.com>
2023-11-30 17:04:52 +0100

    Update __init__.py

---
+++ pastas/plotting/__init__.py
@@ -0,0 +1,15 @@
+"""The plotting sub-package contains plot methods and tools for Pastas.
+
+.. currentmodule:: pastas.plotting
+
+.. autosummary::
+    :toctree: ./generated
+    :nosignatures:
+
+    plots
+    modelplots
+    modelcompare
+    plotutil
+    plotly
+
+"""
