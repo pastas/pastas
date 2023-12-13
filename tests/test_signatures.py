a59a82c29c3edb12ff8d6bd92268e584808b4ac0
Raoul Collenteur <raoulcollenteur@gmail.com>
2023-12-13 18:07:53 +0100

    Update test_signatures.py

--- tests/test_signatures.py
+++ tests/test_signatures.py
@@ -9,3 +9,33 @@ def test_summary():
     idx = date_range("2000", "2010")
     head = Series(index=idx, data=np.random.rand(len(idx)), dtype=float)
     ps.stats.signatures.summary(head)
+
+def test_date_min():
+    """Test the date_min signature to have a mean of 1 if the head is at a minimum at 
+    the first of january of every year in a time series.
+    """
+    idx = date_range("2000", "2010")
+    head = Series(index=idx, data=np.random.rand(len(idx)), dtype=float)
+    head[head.index.dayofyear == 1] = head.min()
+    assert round(ps.stats.signatures.date_min(head)) == 1
+
+def test_date_max():
+    """Test the date_max signature to have a mean of 1 if the head is at a maximum at 
+    the first of january of every year in a time series.
+    """
+    idx = date_range("2000", "2010")
+    head = Series(index=idx, data=np.random.rand(len(idx)), dtype=float)
+    head[head.index.dayofyear == 1] = head.max()
+    assert round(ps.stats.signatures.date_max(head)) == 1
+
+def test_date_mean():
+    """Test the date_mean signature to have a mean of 1 if the head is at a mean at 
+    the first of january of every year in a time series.
+    """
+    idx = date_range("2000", "2010")
+    head = Series(index=idx, data=np.random.rand(len(idx)), dtype=float)
+    head[head.index.dayofyear == 1] = head.mean()
+    assert round(ps.stats.signatures.date_mean(head)) == 1
+
+
+
