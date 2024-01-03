84ca7cc266752d8aefd35e18486c5c4d687c65ed
Davíd Brakenhoff <d.brakenhoff@artesia-water.nl>
2024-01-03 16:24:37 +0100

    fix HantushWellModel checks in code (redo of PR#643) - use isinstance checks instead of checks on name

--- pastas/model.py
+++ pastas/model.py
@@ -27,6 +27,7 @@ from pastas.io.base import _load_model, dump
 from pastas.modelstats import Statistics
 from pastas.noisemodels import NoiseModel
 from pastas.plotting.modelplots import Plotting, _table_formatter_stderr
+from pastas.rfunc import HantushWellModel
 from pastas.solver import LeastSquares
 from pastas.stressmodels import Constant
 from pastas.timeseries import TimeSeries
@@ -1632,7 +1633,7 @@ class Model:
         else:
             if p is None:
                 p = self.get_parameters(name)
-            if self.stressmodels[name].rfunc._name == "HantushWellModel":
+            if isinstance(self.stressmodels[name].rfunc, HantushWellModel):
                 kwargs = {"warn": warn}
             else:
                 kwargs = {}
@@ -1916,8 +1917,8 @@ class Model:
         )
         check["len_oseries_calib"] = len_oseries_calib
 
-        for sm_name in sm_names:
-            if self.stressmodels[sm_name].rfunc._name == "HantushWellModel":
+        for sm_name in self.stressmodels:
+            if isinstance(self.stressmodels[sm_name].rfunc, HantushWellModel):
                 kwargs = {"warn": False}
             else:
                 kwargs = {}
--- pastas/plotting/modelplots.py
+++ pastas/plotting/modelplots.py
@@ -19,6 +19,7 @@ from pastas.plotting.plotutil import (
     _table_formatter_params,
     _table_formatter_stderr,
 )
+from pastas.rfunc import HantushWellModel
 from pastas.typing import Axes, Figure, Model, TimestampType
 
 logger = logging.getLogger(__name__)
@@ -267,7 +268,7 @@ class Plotting:
             # plot the step response
             rkwargs = {}
             if self.ml.stressmodels[sm_name].rfunc is not None:
-                if self.ml.stressmodels[sm_name].rfunc._name == "HantushWellModel":
+                if isinstance(self.ml.stressmodels[sm_name].rfunc, HantushWellModel):
                     rkwargs = {"warn": False}
             response = self.ml._get_response(
                 block_or_step=block_or_step, name=sm_name, add_0=True, **rkwargs
