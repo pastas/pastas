074ae95536d651e734c940768bf648e104932da5
wjz <willem_jan.zaadnoordijk@tno.nl>
2025-02-07 14:38:38 +0100

    added optional aggregation period to function for SGI calculation and first implementation to test

--- pastas/stats/sgi.py
+++ pastas/stats/sgi.py
@@ -1,17 +1,21 @@
 """This module contains methods to compute the Standardized Groundwater Index."""
 
-from numpy import linspace
+from numpy import linspace, array
 from pandas import DataFrame, Series
 from scipy.stats import norm
 
 
-def sgi(series: Series) -> Series:
-    """Method to compute the Standardized Groundwater Index
+def sgi(series: Series, period=1) -> Series:
+    """Method to compute the Standardized Groundwater Index (SGI)
     :cite:t:`bloomfield_analysis_2013`.
 
     Parameters
     ----------
     series: pandas.Series or Pandas.DataFrame
+        Pandas time series of the groundwater levels
+        for which the SGI is to be determined
+    period: integer, optional
+        Length of the aggredation period in months (default: 1; allowed: 1, 2, 3)
 
     Returns
     -------
@@ -22,20 +26,24 @@ def sgi(series: Series) -> Series:
     Notes
     -----
     The Standardized Groundwater Index (SGI) is a non-parametric method to
-    standardize groundwater levels. The SGI is calculated for each month
-    separately. The SGI is a dimensionless index and is used to compare
-    groundwater levels across different wells. It is generally recommended to resample
-    the data to a monthly time series before computing the SGI.
-
+    standardize groundwater levels. The SGI is calculated for each aggregation
+    period within the year separately.
+    The SGI is a dimensionless index and is used to compare groundwater levels
+    across different wells. It may be useful to resample the time series to a
+    monthly interval before computing the SGI.
     """
+    if period != 1 and period != 2 and period !=3 :
+        raise Exception("SGI can only be called with period = 1, 2, or 3; not"+
+                        str(period))
     if isinstance(series, DataFrame):
         series = series.apply(sgi)
     elif isinstance(series, Series):
         series = series.dropna().copy()  # Create a copy to ensure series is untouched.
 
         # Loop over the months
-        for month in range(1, 13):
-            data = series[series.index.month == month]
+        for month in range(1, 13, period):
+            sel = array(range(period))+month
+            data = series[series.index.month in sel]
             n = data.size  # Number of observations
             pmin = 1 / (2 * n)
             pmax = 1 - pmin
