9d77ef0109f31022f32aee13614f4ed386e7c410
wjz <willem_jan.zaadnoordijk@tno.nl>
2025-02-21 16:11:51 +0100

    ruff clean (with bare except not fixed) and format test_sgi.py

--- tests/test_sgi.py
+++ tests/test_sgi.py
@@ -3,46 +3,47 @@ from matplotlib import pyplot as plt
 
 import pastas as ps
 
-
 fileurl = "https://raw.githubusercontent.com/pastas/pastas/master/doc/examples/data"
 dataset = pd.read_csv(f"{fileurl}/head_nb1.csv", index_col=0, parse_dates=True)
 
-series = pd.Series(dataset['head']*100., dtype='int64')
+series = pd.Series(dataset["head"] * 100.0, dtype="int64")
 
 
-plt.figure(figsize=(20,12))
+plt.figure(figsize=(20, 12))
 # original series
-plt.subplot(2,1,1)
-plt.title('Original groundwater head time series')
+plt.subplot(2, 1, 1)
+plt.title("Original groundwater head time series")
 plt.xlim(dataset.index.values[0], dataset.index.values[-1])
-plt.plot(dataset.index.values, dataset['head'].values, 'k')
-plt.ylabel('groundwater head')
+plt.plot(dataset.index.values, dataset["head"].values, "k")
+plt.ylabel("groundwater head")
 plt.grid(True)
 # SGIs
-plt.subplot(2,1,2)
-plt.title('Standardized Groundwater Index')
-plt.ylim(-3,3)
-for period in [0,1,2,3,4,'a'] :
-    aLabel = 'SGI-'+str(period)
+plt.subplot(2, 1, 2)
+plt.title("Standardized Groundwater Index")
+plt.ylim(-3, 3)
+for period in [0, 1, 2, 3, 4, "a"]:
+    aLabel = "SGI-" + str(period)
     sgiArr = period
-    try :
+    try:
         # with dataset
-        aLab = aLabel+'df'
+        aLab = aLabel + "df"
         sgiArr = ps.stats.sgi(dataset, period=period)
         plt.plot(sgiArr.index.values, sgiArr.values, label=aLab)
         # with Series
-        aLab = aLabel+'series'
+        aLab = aLabel + "series"
         sgiArr = ps.stats.sgi(series, period=period)
-        plt.plot(sgiArr.index.values, sgiArr.values, ls=':', label=aLab)
-    except :
-        if period in [1,2,3] :
-            raise Exception('sgi should work for period='+str(period))
-    if period == 1 :
-        print(sgiArr[1],-0.295179)
-        assert abs(sgiArr[1]- -0.295179) < 0.001
+        plt.plot(sgiArr.index.values, sgiArr.values, ls=":", label=aLab)
+    except:
+        # ruff check   gives error for line above
+        #              E722 Do not use bare `except`
+        if period in [1, 2, 3]:
+            raise Exception("sgi should work for period=" + str(period))
+    if period == 1:
+        print(sgiArr[1], -0.295179)
+        assert abs(sgiArr[1] - -0.295179) < 0.001
 
 plt.xlim(dataset.index.values[0], dataset.index.values[-1])
-plt.ylabel('SGI')
+plt.ylabel("SGI")
 plt.grid(True)
 plt.legend()
 plt.show()
