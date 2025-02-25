bb93f66d52ab05ae891a1e32e8f6b24ed751fb7b
wjz <willem_jan.zaadnoordijk@tno.nl>
2025-02-25 09:42:21 +0100

    adaptation of test_sgi.py to review of pull request https://github.com/pastas/pastas/pull/892

--- tests/test_sgi.py
+++ tests/test_sgi.py
@@ -7,22 +7,22 @@ tol = 1e-3
 
 
 def test_sgi_period1(head: Series) -> None:
-    sgi = ps.stats.sgi(head, period=1)
+    sgi = ps.stats.sgi(head, timescaleMonths=1)
     assert pytest.approx(sgi.iat[30], tol) == -0.812
 
 
 def test_sgi_df(head: Series) -> None:
     df = concat([head, head], axis=1)
-    sgi = ps.stats.sgi(df, period=1)
+    sgi = ps.stats.sgi(df, timescaleMonths=1)
     assert pytest.approx(sgi.iat[30, 0], tol) == -0.812
     assert pytest.approx(sgi.iat[30, 1], tol) == -0.812
 
 
 def test_sgi_period2(head: Series) -> None:
-    sgi = ps.stats.sgi(head, period=2)
+    sgi = ps.stats.sgi(head, timescaleMonths=2)
     assert pytest.approx(sgi.iat[30], tol) == -1.258
 
 
 def test_sgi_perioderror(head: Series) -> None:
     with pytest.raises(ValueError):
-        _ = ps.stats.sgi(head, period=4)
+        _ = ps.stats.sgi(head, timescaleMonths=4)
