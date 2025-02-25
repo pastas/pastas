86724e6d87fe1a5ab48d66fa38c5ceb51aee9a94
wjz <willem_jan.zaadnoordijk@tno.nl>
2025-02-25 09:29:33 +0100

    changes in response to review of pull request https://github.com/pastas/pastas/pull/892

--- pastas/stats/sgi.py
+++ pastas/stats/sgi.py
@@ -5,7 +5,7 @@ from pandas import DataFrame, Series
 from scipy.stats import norm
 
 
-def sgi(series: Series, period=1) -> Series:
+def sgi(series: Series, timescaleMonths: int = 1) -> Series:
     """Method to compute the Standardized Groundwater Index (SGI)
     :cite:t:`bloomfield_analysis_2013`.
 
@@ -14,7 +14,7 @@ def sgi(series: Series, period=1) -> Series:
     series: pandas.Series or Pandas.DataFrame
         Pandas time series of the groundwater levels
         for which the SGI is to be determined
-    period: integer, optional
+    timescaleMonths: integer, optional
         Length of the aggredation period in months (default: 1; allowed: 1, 2, 3)
 
     Returns
@@ -27,28 +27,27 @@ def sgi(series: Series, period=1) -> Series:
     -----
     The Standardized Groundwater Index (SGI) is a non-parametric method to
     standardize groundwater levels. The SGI is calculated for each aggregation
-    period within the year separately.
+    period within the year separately. The data within that period in all years
+    in the series are used to determine the reference for which the index is
+    calculated for each value in that period.
     The SGI is a dimensionless index and is used to compare groundwater levels
     across different wells. It may be useful to resample the time series to a
     monthly interval before computing the SGI.
     """
-    if period != 1 and period != 2 and period != 3:
-        raise Exception(
-            "SGI can only be called with period = 1, 2, or 3; not" + str(period)
+    if timescaleMonths not in (1, 2, 3):
+        raise ValueError(
+            "SGI can only be called with timescaleMonths = 1, 2, or 3; not" + str(timescaleMonths)
         )
     if isinstance(series, DataFrame):
-        series = series.apply(sgi, period=period)
+        series = series.apply(sgi, timescaleMonths=timescaleMonths)
     elif isinstance(series, Series):
         # Create a copy to ensure series is untouched.
         # Set dtype to avoid conflict when assinging SGI values
-        series = Series(
-            data=series.values, index=series.index, dtype="float64", copy=True
-        )
-        series.dropna(inplace=True)
+        series = series.copy().dropna(inplace=True).astype(float)
 
         # Loop over the months
-        for month in range(1, 13, period):
-            sel = array(range(period)) + month
+        for month in range(1, 13, timescaleMonths):
+            sel = array(range(timescaleMonths)) + month
             data = series[series.index.month.isin(sel)]
             n = data.size  # Number of observations
             pmin = 1 / (2 * n)
