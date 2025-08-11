3824c41f88409df4351dbd80cc74749b0788677a
martinvonk <vonk.mart@gmail.com>
2025-08-11 16:24:44 +0200

    ruff

--- pastas/stats/__init__.py
+++ pastas/stats/__init__.py
@@ -19,7 +19,7 @@ import pastas.stats.metrics as metrics
 import pastas.stats.signatures as signatures
 
 from .core import acf, ccf, mean, std, var
-from .dutch import ghg, glg, gvg, q_ghg, q_glg, q_gvg, gg
+from .dutch import gg, ghg, glg, gvg, q_ghg, q_glg, q_gvg
 from .metrics import (
     aic,
     bic,
@@ -27,13 +27,13 @@ from .metrics import (
     kge,
     kge_2012,
     mae,
-    nse,
     nnse,
+    nse,
     pearsonr,
+    picp,
     rmse,
     rsq,
     sse,
-    picp,
 )
 from .sgi import sgi
 from .tests import diagnostics, durbin_watson, ljung_box, runs_test, stoffer_toloi
