edeb82ab88a091513e2481dab85f53dfe9ea36bb
martinvonk <vonk.mart@gmail.com>
2026-05-05 17:10:23 +0200

    update doc

--- pastas/solver/least_squares.py
+++ pastas/solver/least_squares.py
@@ -488,8 +488,17 @@ class BaseLeastSquares(BaseSolver):
         noise: Boolean
             If True, minimizes the sum of squared noise computed by the NoiseModel.
         weights: pandas.Series, optional
-            pandas Series by which the residual or noise series are
-            multiplied. Typically values between 0 and 1.
+            A pandas Series used to scale the residual or noise (in the case
+            of a `NoiseModel`) during optimization. The weights must share
+            the same `DateTimeIndex` as the observations (`ml.observations()`)
+            to ensure proper alignment. These weights are applied such that
+            the minimized objective function in least-squares solvers is
+            ``sum((weights * residuals)**2)``. This means that a residual with
+            double the weight has four times as much influence. If None, equal
+            weights are used. This can be used to put extra/less weight on certain
+            periods (e.g., droughts) or measurements (i.e. outliers), and make more
+            complex calibration schemes (see, for example,
+            :cite:`colllenteur_analysis_2023`).
         callback: ufunc, optional
             function that is called after each iteration. the parameters are
             provided to the func. E.g. "callback(parameters)"
