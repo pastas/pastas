59607cdf4f012c25b48b57c738ef615a7b4b8241
Raoul Collenteur <raoulcollenteur@gmail.com>
2026-05-15 12:33:51 +0200

    Revert "Restructured this PR"
    This reverts commit f7865938b22ad3545f84e10219740aa9ea555d82.

--- pastas/plotting/modelplots.py
+++ pastas/plotting/modelplots.py
@@ -134,7 +134,7 @@ class Plotting:
         tmax: Timestamp | str | None = None,
         figsize: tuple = (10, 8),
         split_contributions: bool = False,
-        all_responses: bool = False,
+        all_responses: bool = True,
         adjust_height: bool = True,
         return_warmup: bool = False,
         block_or_step: Literal["block", "step"] = "step",
@@ -301,8 +301,11 @@ class Plotting:
                     sm=sm,
                     block_or_step=block_or_step,
                     ax=ax_response,
-                    all_responses=all_responses,
-                    istress=istress if split_contributions else None,
+                    istress=(
+                        istress
+                        if split_contributions
+                        else (None if all_responses else 0)
+                    ),
                 )
                 ax_response_xlim = ax_response.get_xlim()
                 rmax = max(rmax, ax_response_xlim[1])
@@ -345,7 +348,7 @@ class Plotting:
         tmin: Timestamp | str | None = None,
         tmax: Timestamp | str | None = None,
         split_contributions: bool = False,
-        all_responses: bool = False,
+        all_responses: bool = True,
         stderr: bool = False,
         block_or_step: Literal["block", "step"] = "step",
         return_warmup: bool = False,
@@ -536,8 +539,9 @@ class Plotting:
                 sm=sm,
                 block_or_step=block_or_step,
                 ax=axd[rf_key],
-                all_responses=all_responses,
-                istress=istress if split_contributions else None,
+                istress=(
+                    istress if split_contributions else (None if all_responses else 0)
+                ),
             )
 
         # share x-axes of simulation, residuals and contributions
@@ -576,33 +580,12 @@ class Plotting:
         sm: StressModel,
         block_or_step: Literal["step", "block"],
         ax: Axes,
-        all_responses: bool | None = None,
         istress: int | None = None,
     ):
         """Internal method to plot the response of a Stressmodel in the results-plot"""
-        if all_responses is None and istress is None:
-            all_responses = (
-                False
-                if (isinstance(sm, RechargeModel) or istress is not None)
-                else True
-            )
-        if istress is not None:
-            responses = [
-                self.ml._get_response(
-                    block_or_step=block_or_step, name=sm.name, istress=istress
-                )
-            ]
-        elif all_responses:
-            responses = self.ml._get_all_responses(
-                block_or_step=block_or_step, name=sm.name
-            )
-        else:
-            responses = [
-                self.ml._get_response(
-                    block_or_step=block_or_step, name=sm.name, istress=istress
-                )
-            ]
-
+        responses = sm.get_responses(
+            self.ml, block_or_step=block_or_step, istress=istress
+        )
         responses = [x for x in responses if x is not None]
         if responses:
             # Keep the first cycle color for a single response, but reserve it
