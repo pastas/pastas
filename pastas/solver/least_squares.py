fcda712687c7289d7e188a41a1823a81a989b40e
martinvonk <vonk.mart@gmail.com>
2026-05-21 11:14:28 +0200

    fix typo

--- pastas/solver/least_squares.py
+++ pastas/solver/least_squares.py
@@ -1103,7 +1103,7 @@ class LmfitSolve(LeastSquaresBase):
         init_kwargs = [k for k in kwargs if hasattr(self, k)]
         for k in init_kwargs:
             logger.info(f"Setting {k} to {kwargs[k]} for LmfitSolve solver.")
-            setattr(self, k, kwargs.pop[k])
+            setattr(self, k, kwargs.pop(k))
 
         # Deal with the parameters
         parameters = lmfit.Parameters()
