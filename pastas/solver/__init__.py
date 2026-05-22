00319c79aaa919ba5387338c7d8a8eb9aa3884c6
Raoul Collenteur <raoulcollenteur@gmail.com>
2026-05-22 11:14:36 +0200

    Add solvers to the docs

--- pastas/solver/__init__.py
+++ pastas/solver/__init__.py
@@ -1,4 +1,6 @@
 # ruff: noqa: F401
+"""The solver sub-package contains the solvers and options for parameter (uncertainty) estimation for Pastas."""
+
 from .least_squares import LeastSquares, LmfitSolve
 from .likelihood import GaussianLikelihood, GaussianLikelihoodAr1
 from .mcmc import EmceeSolve
