dafe1fc8737ea3b458302c813f32690253b5536f
Ruben Caljé <r.calje@artesia-water.nl>
2026-05-27 13:58:00 +0200

    Rename name to transform in ThresholdTransform

--- pastas/transform.py
+++ pastas/transform.py
@@ -50,7 +50,7 @@ class ThresholdTransform:
         value: float = np.nan,
         vmin: float = np.nan,
         vmax: float = np.nan,
-        name: str = "threshold",
+        name: str = "transform",
         nparam: int = 2,
     ) -> None:
         self.value = value
