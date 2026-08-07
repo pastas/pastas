17115c826eee579fb793516e76b90078112dbb0d
martinvonk <vonk.mart@gmail.com>
2026-08-07 10:25:15 +0200

    raise AttributeError

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -55,13 +55,13 @@ def test_results_kwargs_split_deprecation(ml_noisemodel: Model) -> None:
 
 
 def test_results_mosaic(ml_noisemodel: Model) -> None:
-    with pytest.warns(FutureWarning, match="return_dict"):
+    with pytest.raises(AttributeError, match="return_dict"):
         _ = ml_noisemodel.plots.results_mosaic(stderr=True, all_responses=False)
     plt.close()
 
 
 def test_results_mosaic_split(ml_noisemodel: Model) -> None:
-    with pytest.warns(FutureWarning, match="return_dict"):
+    with pytest.raises(AttributeError, match="return_dict"):
         _ = ml_noisemodel.plots.results_mosaic(split_contributions=True)
     plt.close()
 
